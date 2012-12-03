48180905c6674fb52302637c8526c716e07191fb
Rok Garbas <rok@garbas.si>
2012-12-03 15:41:04 +0100

    dropdowns working

--- src/backend.js
+++ src/backend.js
@@ -5,17 +5,35 @@ var toolbar_path = '../lib/plone.app.toolbar/plone/app/toolbar/resources/';
 requirejs.config({
   paths: {
     'jquery': toolbar_path + 'lib/jquery-1.8.2.min',
-    'jquery.iframe': toolbar_path + 'src/jquery.iframe'
+    'jquery-iframe': toolbar_path + 'src/jquery.iframe',
+    'jquery-mask': toolbar_path + 'src/jquery.mask',
+    'bootstrap-download': toolbar_path + 'lib/bootstrap/js/bootstrap-dropdown',
+    'plone-init': toolbar_path + 'src/plone.init',
+    'plone-toolbar': toolbar_path + 'src/plone.toolbar'
   },
   shim: {
-    'jquery.iframe': {
+    'jquery-iframe': {
       deps: [ 'jquery' ]
+    },
+    'jquery-mask': {
+      deps: [ 'jquery' ]
+    },
+    'bootstrap-download': {
+      deps: [ 'jquery' ]
+    },
+    'plone-init': {
+      deps: [ 'jquery' ]
+    },
+    'plone-toolbar': {
+      deps: [ 'jquery', 'jquery-iframe', 'bootstrap-download' ]
     }
   }
 });
 
 require([
-    'jquery.iframe'
+    'jquery-iframe',
+    'plone-init',
+    'plone-toolbar'
    ], function($, iframe) {
 
    console.log('WORKS!');
