2b0c91da30385523d794f48272dc4501785e0048
Rok Garbas <rok@garbas.si>
2013-01-27 08:44:40 +0100

    initial work on test setup

--- test/buster.js
+++ test/buster.js
@@ -1,15 +1,38 @@
-var _ = require('underscore'),
-    config = module.exports,
-    defaults = {
-        rootPath: "../",
-        environment: "browser",
-        extensions: [ require("buster-coverage"), require("buster-amd") ],
-        "buster-coverage": {
-          coverageExclusions: ["example-resource"],
-          combinedResultsOnly: true
-        }
-      };
+//var _ = require('underscore'),
+//    config = module.exports,
+//    defaults = {
+//        rootPath: "../",
+//        environment: "browser",
+//        extensions: [ require("buster-coverage"), require("buster-amd") ],
+//        "buster-coverage": {
+//          coverageExclusions: ["example-resource"],
+//          combinedResultsOnly: true
+//        }
+//      };
 
+module.exports['pattern.modal.js'] = {
+  environment: "browser",
+  rootPath: "..",
+  libs: [
+    'jam/require.js'
+  ],
+  sources: [
+    'jam/jquery/dist/jquery.js',
+    'jam/logging/src/logging.js',
+    'jam/Patterns/src/core/logger.js',
+    'jam/Patterns/src/compat.js',
+    'jam/Patterns/src/utils.js',
+    'jam/Patterns/src/core/parser.js',
+    'jam/Patterns/src/registry.js',
+    'js/pattern.base.js',
+    'js/pattern.modal.js'
+  ],
+  tests: [
+    'test/pattern.modal-test.js'
+  ],
+  extensions: [ require("buster-amd")]
+  //extensions: [ require("buster-amd"), require("buster-coverage")]
+};
 
 //config['plone.app.toolbar-iframe.js'] = _.extend({}, defaults, {
 //  libs:         [],
@@ -21,19 +44,19 @@ var _ = require('underscore'),
 //  extensions:   [ require("buster-coverage") ]
 //});
 
-config['plone.app.toolbar-jquery.iframe.js'] = _.extend({}, defaults, {
-  libs: [
-    'jam/require.js'
-  ],
-  sources: [
-    'jam/jquery/dist/jquery.js',
-    'js/patterns.js',
-    'js/jquery.iframe.js'
-  ],
-  tests: [
-    'test/jquery.iframe-test.js'
-  ]
-});
+//config['plone.app.toolbar-jquery.iframe.js'] = _.extend({}, defaults, {
+//  libs: [
+//    'jam/require.js'
+//  ],
+//  sources: [
+//    'jam/jquery/dist/jquery.js',
+//    'js/patterns.js',
+//    'js/jquery.iframe.js'
+//  ],
+//  tests: [
+//    'test/jquery.iframe-test.js'
+//  ]
+//});
 
 //config['plone.app.toolbar-plone.overlay.js'] = _.extend({}, defaults, {
 //  autoRun:  false,
---
+++ test/pattern.modal-test.js
@@ -0,0 +1,52 @@
+// tests for modal pattern
+//
+// @author Rok Garbas
+// @version 1.0
+// @licstart  The following is the entire license notice for the JavaScript
+//            code in this page.
+//
+// Copyright (C) 2010 Plone Foundation
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 51
+// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// @licend  The above is the entire license notice for the JavaScript code in
+//          this page.
+//
+
+/*jshint bitwise:true, curly:true, eqeqeq:true, immed:true, latedef:true,
+  newcap:true, noarg:true, noempty:true, nonew:true, plusplus:true,
+  regexp:true, undef:true, strict:true, trailing:true, browser:true */
+/*global buster:false, define:false, describe:false, it:false, expect:false */
+
+
+buster.spec.expose();
+define([
+  'jquery',
+  'js/pattern.modal'
+], function($, Modal, undefined) {
+  "use strict";
+
+  describe("Modal", function () {
+    it("", function () {
+      var $el = $('<div>Inline Modal</div>'),
+          modal = new Modal($el);
+      expect(modal.options).toEqual({});
+      expect(modal.$modal).toEqual(modal.$el);
+    });
+    it("should be also avaliable as jquery plugin", function() {
+      var $el = $(
+    });
+  });
+
+});
