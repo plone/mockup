6b76f8287f3524b3d3af2cefbf139f4c9c456049
Rok Garbas <rok@garbas.si>
2013-01-27 08:08:25 +0100

    moving javascript code out of html

---
+++ js/mockup.widgets.js
@@ -0,0 +1,19 @@
+require([
+  'jquery',
+  'sinon',
+  'js/widgets'
+], function($, sinon) {
+
+  // before demo patterns in overlay remove html created by autotoc pattern
+  $('#modal1').on('show.modal.patterns', function(e, modal) {
+    $('.autotoc-nav', modal.$modal).remove();
+  });
+
+  var server = sinon.fakeServer.create();
+  server.autoRespond = true;
+  server.autoRespondAfter = 4000;
+  server.respondWith(/something.html/, function (xhr, id) {
+    xhr.respond(200, { "Content-Type": "html/html" }, $('#something-html').html());
+  });
+
+});
