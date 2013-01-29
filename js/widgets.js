4bb3c43b1b32610fee4bbe8658241ac0c02750f4
Rok Garbas <rok@garbas.si>
2013-01-29 03:27:54 +0100

    updated widget so it works with changes made to registry

--- js/widgets.js
+++ js/widgets.js
@@ -1,7 +1,40 @@
+// Author: Rok Garbas
+// Contact: rok@garbas.si
+// Version: 1.0
+//
+// Description:
+// 
+// License:
+//
+// Copyright (C) 2010 Plone Foundation
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 51
+// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+/*jshint bitwise:true, curly:true, eqeqeq:true, immed:true, latedef:true,
+  newcap:true, noarg:true, noempty:true, nonew:true, plusplus:true,
+  undef:true, strict:true, trailing:true, browser:true, evil:true */
+/*global define:false */
+
 if (window.jQuery) {
-  define( "jquery", [], function () { return window.jQuery; } );
+  define( "jquery", [], function () {
+    "use strict";
+    return window.jQuery;
+  } );
 }
 define([
+  'logging',
   'jquery',
   'jam/Patterns/src/registry',
   'js/pattern.select2',
@@ -9,23 +42,26 @@ define([
   'js/pattern.autotoc',
   'js/pattern.expose',
   'js/pattern.modal'
-], function($, registry) {
+], function(logging, $, registry) {
+  "use strict";
+
+  registry.register({
+    name: "plone-widgets",
+    transform: function($root) {
+
+      // enableFormTabbing
+      var $match = $root.filter('.enableFormTabbing');
+      $match = $match.add($root.find('.enableFormTabbing'));
+      $match.addClass('pat-autotoc');
+      $match.attr('data-pat-autotoc', 'levels:legend;section:fieldset;klass:autotabs');
+
+    }
+
+  });
 
   // Initial initialization of patterns
   $(document).ready(function() {
     registry.scan($('body'));
-  });
-
-
-  // 
-  $(document).on("patterns-registry-before-scan.patterns", function(e) {
-    var $el = $(e.target);
-
-    // enableFormTabbing
-    $match = $el.filter('.enableFormTabbing');
-    $match = $match.add($el.find('.enableFormTabbing'));
-    $match.addClass('pat-autotoc');
-
   });
 
 });
