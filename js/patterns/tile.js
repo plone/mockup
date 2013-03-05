b3c654d18668cd5a76f17f00a6746ed34ee07e79
Joel Kleier <joel.kleier@gmail.com>
2013-03-05 08:42:50 -0600

    added basic template for the Tile pattern

---
+++ js/patterns/tile.js
@@ -0,0 +1,18 @@
+define([
+  'jquery',
+  'js/patterns/base'
+], function($, Base, Parser) {
+  "use strict";
+
+  var Tile = Base.extend({
+    name: "tile",
+    defaults: {
+    },
+    init: function() {
+      var self = this;
+    }
+  });
+
+  return AutoTOC;
+
+});
