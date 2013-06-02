c10951f69f3f17ea3f4575f44453ab86d1035e1e
Franco Pellegrini <frapell@gmail.com>
2013-06-02 17:41:37 -0300

    Add a tablesorter pattern.

--- test/patterns-test.js
+++ test/patterns-test.js
@@ -48,6 +48,7 @@ define([
   'js/patterns/accessibility',
   'js/patterns/cookiedirective',
   'js/patterns/relateditems',
+  'js/patterns/tablesorter',
   'jam/jquery-cookie/jquery.cookie',
 ], function(chai, $, registry,
       Base, AutoTOC, Backdrop,
@@ -99,6 +100,118 @@ define([
     // TODO: make sure that pattern is not initialized twice if scanned twice
   });
   
+          
+/* ==========================
+   TEST: TableSorter
+  ========================== */
+
+  describe("TableSorter", function () {
+    beforeEach(function() {
+      this.$el = $('' +
+        '<table class="pat-tablesorter">'+
+        '   <thead>'+
+        '     <tr>'+
+        '       <th>First Name</th>'+
+        '       <th>Last Name</th>'+
+        '       <th>Number</th>'+
+        '     </tr>'+
+        '   </thead>'+
+        '   <tbody>'+
+        '     <tr>'+
+        '       <td>AAA</td>'+
+        '       <td>ZZZ</td>'+
+        '       <td>3</td>'+
+        '     </tr>'+
+        '     <tr>'+
+        '       <td>BBB</td>'+
+        '       <td>YYY</td>'+
+        '       <td>1</td>'+
+        '     </tr>'+
+        '     <tr>'+
+        '       <td>CCC</td>'+
+        '       <td>XXX</td>'+
+        '       <td>2</td>'+
+        '     </tr>'+
+        '   </tbody>'+
+        ' </table>');
+    });
+    it("test headers have the sort arrow", function() {
+      registry.scan(this.$el);
+      expect(this.$el.find('.sortdirection').size()).to.equal(3);
+    });
+    it("test sort by second column", function() {
+      registry.scan(this.$el);
+      this.$el.find('thead th').eq(1).trigger("click");
+
+      var should_be = ["CCC", "BBB", "AAA"];
+      var elem;
+      for (var i=0;i<should_be.length;i++){
+        // We are checking first td of each tr of tbody, just to see the
+        // order
+        elem = this.$el.find('tbody tr td').eq(i*3);
+        expect(elem.text()).to.equal(should_be[i]);
+      }
+
+      var trs = this.$el.find('tbody tr');
+      expect(trs.eq(0).hasClass('odd')).to.be.true;
+      expect(trs.eq(1).hasClass('odd')).to.be.false;
+      expect(trs.eq(2).hasClass('odd')).to.be.true;
+      expect(trs.eq(0).hasClass('even')).to.be.false;
+      expect(trs.eq(1).hasClass('even')).to.be.true;
+      expect(trs.eq(2).hasClass('even')).to.be.false;
+
+    });
+    it("test sort by third column", function() {
+      registry.scan(this.$el);
+      this.$el.find('thead th').eq(2).trigger("click");
+
+      var should_be = ["BBB", "CCC", "AAA"];
+      var elem;
+      for (var i=0;i<should_be.length;i++){
+        // We are checking first td of each tr of tbody, just to see the
+        // order
+        elem = this.$el.find('tbody tr td').eq(i*3);
+        expect(elem.text()).to.equal(should_be[i]);
+      }
+
+      var trs = this.$el.find('tbody tr');
+      expect(trs.eq(0).hasClass('odd')).to.be.true;
+      expect(trs.eq(1).hasClass('odd')).to.be.false;
+      expect(trs.eq(2).hasClass('odd')).to.be.true;
+      expect(trs.eq(0).hasClass('even')).to.be.false;
+      expect(trs.eq(1).hasClass('even')).to.be.true;
+      expect(trs.eq(2).hasClass('even')).to.be.false;
+
+    });
+    it("test several sorts and finally back to first column", function() {
+      registry.scan(this.$el);
+      this.$el.find('thead th').eq(2).trigger("click");
+      this.$el.find('thead th').eq(3).trigger("click");
+      this.$el.find('thead th').eq(2).trigger("click");
+      this.$el.find('thead th').eq(1).trigger("click");
+      this.$el.find('thead th').eq(3).trigger("click");
+      this.$el.find('thead th').eq(1).trigger("click");
+
+      var should_be = ["AAA", "BBB", "CCC"];
+      var elem;
+      for (var i=0;i<should_be.length;i++){
+        // We are checking first td of each tr of tbody, just to see the
+        // order
+        elem = this.$el.find('tbody tr td').eq(i*3);
+        expect(elem.text()).to.equal(should_be[i]);
+      }
+
+      var trs = this.$el.find('tbody tr');
+      expect(trs.eq(0).hasClass('odd')).to.be.true;
+      expect(trs.eq(1).hasClass('odd')).to.be.false;
+      expect(trs.eq(2).hasClass('odd')).to.be.true;
+      expect(trs.eq(0).hasClass('even')).to.be.false;
+      expect(trs.eq(1).hasClass('even')).to.be.true;
+      expect(trs.eq(2).hasClass('even')).to.be.false;
+
+    });
+  });
+
 /* ==========================
    TEST: CookieDirective
   ========================== */
