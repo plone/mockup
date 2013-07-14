5194fc67734ace2208ddd6571c8e9d4ab344601c
Rok Garbas <rok@garbas.si>
2013-07-14 07:36:43 +0200

    adding more bower resources

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,18 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       all: ['Gruntfile.js', 'js/**/*.js', 'test/*.js']
+    },
+    bower: {
+      target: {
+        rjsConfig: 'js/config.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bower-requirejs');
 
   grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['bower']);
+
 };
--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 GIT = git
 NPM = npm
 GRUNT = ./node_modules/.bin/grunt
+BOWER = ./node_modules/.bin/bower
 
 bootstrap:
 	mkdir -p build
 	if test ! -d docs; then $(GIT) clone git://github.com/plone/mockup.git -b gh-pages docs; fi
 	$(NPM) link
+	$(BOWER) install
+	$(GRUNT) bower
 
 test:
 	$(GRUNT) test
