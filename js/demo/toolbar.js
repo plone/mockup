7f24632a7a4b055f3b4ba8fa26857873ec6a38ac
Joel Kleier <joel.kleier@gmail.com>
2013-07-13 23:47:02 -0400

    added test html and a click toggle for items in toggle menus in the toolbar

--- js/demo/toolbar.js
+++ js/demo/toolbar.js
@@ -21,7 +21,7 @@ require([
   }
   $('script[type="text/x-ajax-respond"]', window.parent.document).each(function() {
     var $el = $(this);
-    server.respondWith($el.data('url'), function (xhr) {
+    server.respondWith($el.data('url'), function(xhr) {
       var $body = $('<div/>').append($($el.html()).filter('#portal-columns')),
           request = deserialize(xhr.requestBody);
       if ($el.data('url') === 'sharing.html') {
@@ -53,6 +53,10 @@ require([
           '<html><body>' + $body.html() + '</body></html>');
     });
     server.respond();
+  });
+
+  $(document).ready(function() {
+    registry.scan($('body'));
   });
 
 });
