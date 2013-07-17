6f9d9117b060daf7ce29095b6558cf48d487f5fa
David Blewett <davidb@sixfeetup.com>
2013-07-17 17:17:24 -0400

    Clarify regex literal.

--- tests/config.js
+++ tests/config.js
@@ -1,6 +1,6 @@
 var tests = Object.keys(window.__karma__.files).filter(function (file) {
       console.log(file);
-      return /\-test\.js$/.test(file);
+      return (/\-test\.js$/).test(file);
 });
 
 requirejs.config({
