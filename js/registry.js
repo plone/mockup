188f79c2cb9345c59740f52395ea0a25d649a2eb
Rok Garbas <rok@garbas.si>
2013-09-24 19:09:23 +0200

    stripping it down to minimal

--- js/registry.js
+++ js/registry.js
@@ -1,275 +1,83 @@
-/**
- COPIED FROM Patterns @commit c0f34bf2bbfb0d6e65eebf3caad4b09bc8e7b5d3
- WITH CHANGES by Ryan Foster
-
- * Patterns registry - Central registry and scan logic for patterns
- *
- * Copyright 2012-2013 Simplon B.V.
- * Copyright 2012-2013 Florian Friesdorf
- * Copyright 2013 Marko Durkovic
- * Copyright 2013 Rok Garbas
- */
-
-/*
- * changes to previous patterns.register/scan mechanism
- * - if you want initialised class, do it in init
- * - init returns set of elements actually initialised
- * - handle once within init
- * - no turnstile anymore
- * - set pattern.jquery_plugin if you want it
- */
 define([
-    "jquery"
-], function($) {
+  "jquery"
+], function($, undefined) {
 
-    // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/forEach (JS 1.6)
-    // Production steps of ECMA-262, Edition 5, 15.4.4.18
-    // Reference: http://es5.github.com/#x15.4.4.18
-    if ( !Array.prototype.forEach ) {
+  function warn(msg) {
+    if (window.DEBUG) {
+      console.warn(msg);
+    }
+  }
 
-        Array.prototype.forEach = function( callback, thisArg ) {
+  var registry = {
 
-            var T, k;
+    patterns: {},
 
-            if ( this === null ) {
-                throw new TypeError( " this is null or not defined" );
+    init: function($el, patternName, options) {
+      $el.data('pattern-' + patternName,
+          new registry.patterns[patternName]($el, options));
+    },
+
+    scan: function(content, do_not_catch_init_exception) {
+      var $content = $(content),
+          patterns = [];
+
+      patterns.push($content.filter('[class^="pat-"]'));
+      patterns.push($('[class^="pat-"]', $content));
+
+      $.each(patterns, function(i, $el) {
+        $.each($el.attr('class').split(' '), function(j, className) {
+          if (className.indexOf('pat-')) {
+            try {
+              registry.init($el, className.substr(4));
+            } catch (e) {
+              warn('Failed while initializing "' + className.substr(4) +
+                '" on element: ' + $el);
+              // TODO: provide some nicer traceback what went wrong
+            }
+          }
+        });
+      });
+
+    },
+
+    register: function(Pattern) {
+
+      // automatically create jquery plugin from pattern
+      if (Pattern.prototype.jqueryPlugin === undefined) {
+        Pattern.prototype.jqueryPlugin = "pattern" +
+            Pattern.prototype.name.charAt(0).toUpperCase() +
+            Pattern.prototype.name.slice(1);
+      }
+
+      if (Pattern.prototype.jqueryPlugin) {
+        $.fn[Pattern.prototype.jqueryPlugin] = function(method, options) {
+          $(this).each(function() {
+            var $el = $(this),
+                pattern = $el.data('pattern-' + Pattern.prototype.name);
+            if (typeof method === "object") {
+              options = method;
+              method = undefined;
+            }
+            if (!pattern || typeof(pattern) === 'string') {
+              registry.init($el, options);
+            } else if (method && pattern && pattern[method]) {
+              if (method.charAt(0) !== '_') {
+                pattern[method].apply(pattern, [options]);
+              } else {
+                console.warn('Method "' + method + '" is private.');
+              }
             }
 
-            // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
-            var O = Object(this);
-
-            // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
-            // 3. Let len be ToUint32(lenValue).
-            var len = O.length >>> 0; // Hack to convert O.length to a UInt32
-
-            // 4. If IsCallable(callback) is false, throw a TypeError exception.
-            // See: http://es5.github.com/#x9.11
-            if ( {}.toString.call(callback) !== "[object Function]" ) {
-                throw new TypeError( callback + " is not a function" );
-            }
-
-            // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-            if ( thisArg ) {
-                T = thisArg;
-            }
-
-            // 6. Let k be 0
-            k = 0;
-
-            // 7. Repeat, while k < len
-            while( k < len ) {
-
-                var kValue;
-
-                // a. Let Pk be ToString(k).
-                //   This is implicit for LHS operands of the in operator
-                // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
-                //   This step can be combined with c
-                // c. If kPresent is true, then
-                if ( k in O ) {
-
-                    // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
-                    kValue = O[ k ];
-
-                    // ii. Call the Call internal method of callback with T as the this value and
-                    // argument list containing kValue, k, and O.
-                    callback.call( T, kValue, k, O );
-                }
-                // d. Increase k by 1.
-                k++;
-            }
-            // 8. return undefined
+          });
+          return this;
         };
-    }
-    // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/ReduceRight (JS 1.8)
-    if (!Array.prototype.reduceRight)
-    {
-        Array.prototype.reduceRight = function(callbackfn /*, initialValue */)
-        {
-            "use strict";
+      }
 
-            if (this === null)
-                throw new TypeError();
-
-            var t = Object(this);
-            var len = t.length >>> 0;
-            if (typeof callbackfn !== "function")
-                throw new TypeError();
-
-            // no value to return if no initial value, empty array
-            if (len === 0 && arguments.length === 1)
-                throw new TypeError();
-
-            var k = len - 1;
-            var accumulator;
-            if (arguments.length >= 2)
-            {
-                accumulator = arguments[1];
-            }
-            else
-            {
-                do
-                {
-                    if (k in this)
-                    {
-                        accumulator = this[k--];
-                        break;
-                    }
-
-                    // if array contains no values, no initial value to return
-                    if (--k < 0)
-                        throw new TypeError();
-                }
-                while (true);
-            }
-
-            while (k >= 0)
-            {
-                if (k in t)
-                    accumulator = callbackfn.call(undefined, accumulator, t[k], k, t);
-                k--;
-            }
-
-            return accumulator;
-        };
-    }
-
-    // https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/keys
-    if (!Object.keys) {
-        Object.keys = (function () {
-            var _hasOwnProperty = Object.prototype.hasOwnProperty,
-            hasDontEnumBug = !({toString: null}).propertyIsEnumerable("toString"),
-            dontEnums = [
-            "toString",
-            "toLocaleString",
-            "valueOf",
-            "hasOwnProperty",
-            "isPrototypeOf",
-            "propertyIsEnumerable",
-            "constructor"
-            ],
-            dontEnumsLength = dontEnums.length;
-
-            return function (obj) {
-                if (typeof obj !== "object" && typeof obj !== "function" || obj === null)
-                    throw new TypeError("Object.keys called on non-object");
-
-                var result = [];
-                for (var prop in obj)
-                    if (_hasOwnProperty.call(obj, prop))
-                        result.push(prop);
-
-                if (hasDontEnumBug)
-                    for (var i=0; i < dontEnumsLength; i++)
-                        if (_hasOwnProperty.call(obj, dontEnums[i]))
-                            result.push(dontEnums[i]);
-                return result;
-            };
-        })();
-    }
-    /*** end compat ***/
-
-    var disable_re = /patterns-disable=([^&]+)/g,
-        dont_catch_re = /patterns-dont-catch/g,
-        dont_catch = false,
-        disabled = {}, match;
-
-    while ((match=disable_re.exec(window.location.search)) !== null) {
-        disabled[match[1]] = true;
-    }
-
-    while ((match=dont_catch_re.exec(window.location.search)) !== null) {
-        dont_catch = true;
-    }
-
-    var registry = {
-        patterns: {},
-        // as long as the registry is not initialized, pattern
-        // registration just registers a pattern. Once init is called,
-        // the DOM is scanned. After that registering a new pattern
-        // results in rescanning the DOM only for this pattern.
-        initialized: false,
-        init: function() {
-            $(document).ready(function() {
-                registry.scan(document.body);
-                registry.initialized = true;
-            });
-        },
-
-        scan: function(content, do_not_catch_init_exception, patterns) {
-            var $content = $(content),
-                all = [], allsel,
-                pattern, $match;
-
-            // If no list of patterns was specified, we scan for all
-            // patterns
-            patterns = patterns || Object.keys(registry.patterns);
-
-            // selector for all patterns
-            patterns.forEach(function(name) {
-                if (disabled[name]) {
-                    return;
-                }
-                pattern = registry.patterns[name];
-                if (pattern.transform) {
-                    if (do_not_catch_init_exception || dont_catch) {
-                        pattern.transform($content);
-                    } else {
-                        try {
-                            pattern.transform($content);
-                        } catch (e) {
-                        }
-                    }
-                }
-                if (pattern.trigger) {
-                    all.push(pattern.trigger);
-                }
-            });
-            allsel = all.join(",");
-
-            // Find all elements that belong to any pattern.
-            $match = $content.find('*').addBack().filter(allsel);
-            $match = $match.filter(function() { return $(this).parents('pre').length === 0; });
-            $match = $match.filter(":not(.cant-touch-this)");
-
-            // walk list backwards and initialize patterns inside-out.
-            //
-            // XXX: If patterns would only trigger via classes, we
-            // could iterate over an element classes and trigger
-            // patterns in order.
-            //
-            // Advantages: Order of pattern initialization controled
-            // via order of pat-classes and more efficient.
-            $match.toArray().reduceRight(function(acc, el) {
-                var $el = $(el);
-
-                for (var name in registry.patterns) {
-                    pattern = registry.patterns[name];
-
-                    if ($el.is(pattern.trigger)) {
-
-                        if (do_not_catch_init_exception || dont_catch) {
-                            try {
-                                pattern.init($el);
-
-                            } catch (e) {
-
-                            }
-                        } else {
-                            pattern.init($el);
-
-                        }
-                    }
-                }
-            }, null);
-        },
-
-        // XXX: differentiate between internal and custom patterns
-        // _register vs register
-        register: function(pattern) {
-            if (!pattern.name) {
-                return false;
-            }
+      if (!Pattern.prototype.name) {
+        if (window.DEBUG) {
+        }
+        return false;
+      }
 
             if (registry.patterns[pattern.name]) {
                 return false;
