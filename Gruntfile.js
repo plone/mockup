a1d59912ee331a1ca8b64aeee8cc71aab12c1f58
lpmayos <lpmayos@gmail.com>
2013-10-06 13:05:13 -0300

    tinymce: split in pattern, links and upload js files to separate functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,9 @@ module.exports = function(grunt) {
   karma_files.push({pattern: 'js/patterns/structure/**/*.tmpl', included: false});
   karma_files.push({pattern: 'js/patterns/filemanager/**/*.tmpl', included: false});
   karma_files.push({pattern: 'js/patterns/filemanager/**/*.js', included: false});
-  karma_files.push({pattern: 'js/patterns/tinymce/templates/*.tmpl', included: false});
+  karma_files.push({pattern: 'js/patterns/tinymce/**/*.tmpl', included: false});
+  karma_files.push({pattern: 'js/patterns/tinymce/**/*.js', included: false});
+
 
   karma_files.push('js/config.js');
   karma_files.push('tests/config.js');
