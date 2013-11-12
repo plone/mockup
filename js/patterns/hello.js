9df2055bbddbfc20e078655730ebe5cbcc8a1a70
Peter Lamut <peter.lamut@niteoweb.com>
2013-11-12 23:33:53 +0100

    Hello docs: "Adding Configuration Options to a Pattern" section

--- js/patterns/hello.js
+++ js/patterns/hello.js
@@ -6,9 +6,17 @@ define([
 
     var HelloWorld = Base.extend({
         name: 'helloworld',
+        defaults: {
+            'color': 'black',
+            'bgcolor': 'yellow'
+        },
         init: function () {
             var $label = this.$el;
             $label.text("Hello, world!");
+            $label.css({
+                'color': this.options.color,
+                'background': this.options.bgcolor
+            });
         }
     });
 
