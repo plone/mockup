3f68159106c875135858b29c1e4acf10861a4927
Rok Garbas <rok@garbas.si>
2013-11-18 19:07:07 +0100

    use latest less on development site as well

--- js/bundles/toolbar_develop.js
+++ js/bundles/toolbar_develop.js
@@ -82,7 +82,7 @@ var domready = function (ready) {
 
     var script3 = document.createElement('script');
     script3.setAttribute('type', 'text/javascript');
-    script3.setAttribute('src', '/++resource++mockup/node_modules/grunt-contrib-less/node_modules/less/dist/less-1.4.1.js');
+    script3.setAttribute('src', '/++resource++mockup/node_modules/grunt-contrib-less/node_modules/less/dist/less-1.5.1.js');
     document.getElementsByTagName("head")[0].appendChild(script3);
 
   });
