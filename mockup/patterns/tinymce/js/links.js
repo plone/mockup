edfd4fccf73bcf09430b852c84246cbe4adeaef2
Franco Pellegrini <frapell@gmail.com>
2014-11-20 19:51:23 +0000

    Index val on 'id' not on 0

--- mockup/patterns/tinymce/js/links.js
+++ mockup/patterns/tinymce/js/links.js
@@ -156,7 +156,7 @@ define([
       var self = this;
       var val = self.$select.select2('data');
       if (val && typeof(val) === 'object') {
-        val = val[0];
+        val = val['id'];
       }
       return val;
     },
