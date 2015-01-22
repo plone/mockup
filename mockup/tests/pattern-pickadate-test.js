0838a26088d19feb642821ea4ed11956a92a794b
JC Brand <jc@opkode.com>
2015-01-22 18:53:49 +0100

    Bugfix. Specify pattern to scan for.

--- mockup/tests/pattern-pickadate-test.js
+++ mockup/tests/pattern-pickadate-test.js
@@ -423,7 +423,7 @@ define([
                                           ']}}'
               );
         self.$el.appendTo('body');
-        registry.scan($input);
+        registry.scan($input, ['pickadate']);
 
         // check if visible and data value are set to default
         expect($('.pattern-pickadate-timezone .select2-chosen').text()).to.equal('Enter timezone...');
@@ -439,7 +439,7 @@ define([
                                           ']}}'
               );
         self.$el.appendTo('body');
-        registry.scan($input);
+        registry.scan($input, ['pickadate']);
 
         var $time = $('.pattern-pickadate-timezone');
 
