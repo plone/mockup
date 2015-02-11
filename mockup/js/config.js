e75bcedb9b47fe4d5927ba9b20d76bd691a15b47
JC Brand <jc@opkode.com>
2015-02-11 10:24:07 +0100

    Add path for pat-registry

--- mockup/js/config.js
+++ mockup/js/config.js
@@ -126,6 +126,7 @@
       'pat-compat': 'bower_components/patternslib/src/core/compat',
       'pat-jquery-ext': 'bower_components/patternslib/src/core/jquery-ext',
       'pat-logger': 'bower_components/patternslib/src/core/logger',
+      'pat-registry': 'bower_components/patternslib/src/core/registry',
       'pat-utils': 'bower_components/patternslib/src/core/utils',
       'logging': 'bower_components/logging/src/logging'
     },
