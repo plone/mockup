16890240cb49c9de18853c2d90216888ab4b17eb
Sam Schwartz <obct537@gmail.com>
2015-07-06 12:31:00 -0500

    Moved showLessBuilder method into the lessbuilderview pattern

--- mockup/patterns/thememapper/js/lessbuilderview.js
+++ mockup/patterns/thememapper/js/lessbuilderview.js
@@ -33,7 +33,7 @@ define([
       self.$button = $('#compileBtn', this.$el);
       self.$errorButton = $('#errorBtn', this.$el);
       self.$button.on('click', function() {
-        self.app.showLessBuilder();
+        self.showLessBuilder();
       });
       self.$errorButton.on('click', function() {
         self.start();
@@ -71,7 +71,72 @@ define([
       this.$message.text("");
       this.$error.text(error).show();
       this.$errorButton.show();
-    }
+    },
+    showLessBuilder: function() {
+      var self = this;
+
+      if( self.app.lessPaths['save'] === undefined ) {
+        self.showError("Error: invalid filetype");
+        return false;
+      }
+
+      self.working();
+
+      var config = {
+        less: [ self.app.lessVariableUrl,
+                self.app.lessPaths['less'],
+                self.app.lessUrl]
+      }
+
+      var iframe = new IFrame({
+        name: 'lessc',
+        resources: config.less,
+        callback: self.app.saveThemeCSS,
+        env: self.app,
+        configure: function(iframe){
+          iframe.window.lessErrorReporting = function(what, error, href){
+            if( error.href !== undefined )
+            {
+              self.app.fileManager.ace.editor.scrollToLine(error.line, true);
+              if( error.type == "Name" ) {
+                var reg = new RegExp(".*(@\\S+)\\s.*");
+                var matches = reg.exec(error.message);
+                if( matches !== null ) {
+                  var varName = matches[1];
+                  var result = self.app.fileManager.ace.editor.findAll(varName);
+                }
+              }
+              else {
+                //The line number is always off by 1? (and LESS indexes from 0) so -2
+                self.app.fileManager.ace.editor.moveCursorToPosition({row: error.line - 2, column: error.column});
+                self.app.fileManager.ace.editor.focus();
+              }
+              self.showError(error);
+            }
+          };
+
+          iframe.styles = [];
+        },
+        onLoad: function(self) {
+          less.pageLoadFinished.then(
+            function() {
+              var $ = window.parent.$;
+              var iframe = window.iframe['lessc'];
+              var styles = $('style', iframe.document);
+              var styleBox = $('#styleBox');
+
+              $(styleBox).empty();
+              $(styles).each(function() {
+                styleBox.append(this.innerHTML);
+              });
+
+              iframe.options.callback();
+            }
+          );
+        }
+      });
+
+    },
   });
 
   return LessBuilderView;
