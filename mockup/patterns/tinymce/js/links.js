a4779b3fa141f0b9f1854d53c419f43c3adaa999
David Glick <david@glicksoftware.com>
2016-09-29 23:26:32 -0700

    Integrate caption from description support from https://github.com/plone/mockup/commit/d3d11d08010a2ef5460ce63ae5b90ddf199e7f59

--- mockup/patterns/tinymce/js/links.js
+++ mockup/patterns/tinymce/js/links.js
@@ -483,6 +483,7 @@ define([
         externalImageText: this.options.text.externalImageText,
         altText: this.options.text.alt,
         imageAlignText: this.options.text.imageAlign,
+        captionFromDescriptionText: this.options.text.captionFromDescription,
         captionText: this.options.text.caption,
         scaleText: this.options.text.scale,
         imageScales: this.options.imageScales,
@@ -503,9 +504,10 @@ define([
       self.$subject = $('input[name="subject"]', self.modal.$modal);
 
       self.$alt = $('input[name="alt"]', self.modal.$modal);
-      self.$caption = $('textarea[name="caption"]', self.modal.$modal);
       self.$align = $('select[name="align"]', self.modal.$modal);
       self.$scale = $('select[name="scale"]', self.modal.$modal);
+      self.$captionFromDescription = $('input[name="captionFromDescription"]', self.modal.$modal);
+      self.$caption = $('textarea[name="caption"]', self.modal.$modal);
 
       /* load up all the link types */
       _.each(self.options.linkTypes, function(type) {
@@ -525,6 +527,15 @@ define([
           }
         });
       });
+
+      self.$captionFromDescription.change(function () {
+        if (this.checked) {
+          self.$caption.prop('disabled', true);
+        } else {
+          self.$caption.prop('disabled', false);
+        }
+      });
+
     },
 
     getLinkUrl: function() {
@@ -584,15 +595,21 @@ define([
     updateImage: function(src) {
       var self = this;
       var title = self.$title.val();
+      var captionFromDescription = self.$captionFromDescription.prop('checked')
 
       self.tiny.focus();
       self.tiny.selection.setRng(self.rng);
+
+      var cssclasses = ['image-richtext', self.$align.val()];
+      if (captionFromDescription) {
+        cssclasses.push('captioned');
+      }
 
       var data = $.extend(true, {}, {
         src: src,
         title: title ? title : null,
         alt: self.$alt.val(),
-        'class': self.$align.val() + ' image-richtext',
+        'class': cssclasses.join(' '),
         'data-linkType': self.linkType,
         'data-scale': self.$scale.val(),
       }, self.linkTypes[self.linkType].attributes());
@@ -624,7 +641,7 @@ define([
       var html_inner = self.dom.createHTML('img', data);
       var caption = self.$caption.val();
       var html_string;
-      if (caption) {
+      if (caption && ! captionFromDescription) {
         html_inner += '\n' + self.dom.createHTML('figcaption', {}, caption);
         //html_inner += '\n' + self.dom.createHTML('figcaption', { class: 'mceNonEditable' }, caption);
         html_string = self.dom.createHTML('figure', {}, html_inner);
@@ -778,18 +795,23 @@ define([
           caption = self.selectedElm;
         }
 
-        self.captionElm = caption;
-        if (self.captionElm) {
-          self.$caption.val(self.captionElm.innerHTML);
-        }
-
         self.imgElm = img;
         self.figureElm = figure;
+        self.captionElm = caption;
 
         if (self.imgElm) {
           var src = self.dom.getAttrib(self.imgElm, 'src');
           self.$title.val(self.dom.getAttrib(self.imgElm, 'title'));
           self.$alt.val(self.dom.getAttrib(self.imgElm, 'alt'));
+
+          if ($(self.imgElm).hasClass('captioned')) {
+            self.$captionFromDescription.prop('checked', true);
+            self.$caption.prop('disabled', true);
+          }
+          if (self.captionElm) {
+            self.$caption.val(self.captionElm.innerHTML);
+          }
+
           linkType = self.dom.getAttrib(self.imgElm, 'data-linktype');
           if (linkType) {
             self.linkType = linkType;
