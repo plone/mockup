29a3efac2011804851cc63d7372d216e9eace7af
Asko Soukka <asko.soukka@iki.fi>
2017-02-16 21:37:42 +0200

    Fix anchor links in Mosaic between tiles

--- mockup/patterns/tinymce/js/links.js
+++ mockup/patterns/tinymce/js/links.js
@@ -204,9 +204,14 @@ define([
     init: function() {
       LinkType.prototype.init.call(this);
       this.$select = this.$el.find('select');
-      this.anchorNodes = [];
-      this.anchorData = [];
+      this.anchorNames = [];  // Sorted list of anchor names
+      this.anchorData = {};   // Data by anchor names
       this.populate();
+    },
+
+    load: function(element) {
+      var name = this.tiny.dom.getAttrib(element, 'data-val');
+      this.$select.select2('val', name);
     },
 
     value: function() {
@@ -220,11 +225,15 @@ define([
     populate: function() {
       var self = this;
       self.$select.find('option').remove();
-      self.anchorNodes = [];
-      self.anchorData = [];
-      var node, i, j, name, title;
+      this.anchorNames = [];  // Sorted list of anchor names
+      this.anchorData = {};   // Data by anchor names
+      var nodes, node, i, j, name, title;
 
-      var nodes = self.tiny.dom.select('a.mceItemAnchor,img.mceItemAnchor,a.mce-item-anchor,img.mce-item-anchor');
+      if (self.tiny.inline) {
+        nodes = $('.mce-content-body').find('a.mceItemAnchor,img.mceItemAnchor,a.mce-item-anchor,img.mce-item-anchor');
+      } else {
+        nodes = self.tiny.dom.select('a.mceItemAnchor,img.mceItemAnchor,a.mce-item-anchor,img.mce-item-anchor');
+      }
       for (i = 0; i < nodes.length; i = i + 1) {
         node = nodes[i];
         name = self.tiny.dom.getAttrib(node, 'name');
@@ -232,12 +241,16 @@ define([
           name = self.tiny.dom.getAttrib(node, 'id');
         }
         if (name !== '') {
-          self.anchorNodes.push(node);
-          self.anchorData.push({name: name, title: name});
+          self.anchorNames.push(name);
+          self.anchorData[name] = {node: node, name: name, title: name};
         }
       }
 
-      nodes = self.tiny.dom.select(self.linkModal.options.anchorSelector);
+      if (self.tiny.inline) {
+        nodes = $('.mce-content-body').find(self.linkModal.options.anchorSelector);
+      } else {
+        nodes = self.tiny.dom.select(self.linkModal.options.anchorSelector);
+      }
       if (nodes.length > 0) {
         for (i = 0; i < nodes.length; i = i + 1) {
           node = nodes[i];
@@ -247,27 +260,35 @@ define([
           }
           name = title.toLowerCase().substring(0,1024);
           name = name.replace(/[^a-z0-9]/g, '-');
+          if ($(node).parents('.mosaic-IDublinCore-title-tile').length) {
+            name = 'content';
+          }
           /* okay, ugly, but we need to first check that this anchor isn't already available */
           var found = false;
-          for (j = 0; j < self.anchorNodes.length; j = j + 1) {
-            var anode = self.anchorData[j];
-            if (anode.name === name) {
+          for (j = 0; j < self.anchorNames.length; j = j + 1) {
+            var data = self.anchorData[self.anchorNames[j]];
+            if (data.name === name) {
               found = true;
               // so it's also found, let's update the title to be more presentable
-              anode.title = title;
+              data.title = title;
               break;
             }
           }
           if (!found) {
-            self.anchorData.push({name: name, title: title, newAnchor: true});
-            self.anchorNodes.push(node);
+            self.anchorNames.push(name);
+            self.anchorData[name] = {node: node, name: name, title: title, newAnchor: true};
           }
         }
       }
-      if (self.anchorNodes.length > 0) {
-        for (i = 0; i < self.anchorData.length; i = i + 1) {
-          var data = self.anchorData[i];
-          self.$select.append('<option value="' + i + '">' + data.title + '</option>');
+      if (self.anchorNames.sort) {
+        self.anchorNames = self.anchorNames.sort(function (a, b) {
+          return $(self.anchorData[a].node).offset().top - $(self.anchorData[b].node).offset().top;
+        });
+      }
+      if (self.anchorNames.length > 0) {
+        for (i = 0; i < self.anchorNames.length; i = i + 1) {
+          var data = self.anchorData[self.anchorNames[i]];
+          self.$select.append('<option value="' + data.name + '">' + data.title + '</option>');
         }
       } else {
         self.$select.append('<option>No anchors found..</option>');
@@ -275,8 +296,8 @@ define([
     },
 
     getIndex: function(name) {
-      for (var i = 0; i < this.anchorData.length; i = i + 1) {
-        var data = this.anchorData[i];
+      for (var i = 0; i < this.anchorNames.length; i = i + 1) {
+        var data = this.anchorData[this.anchorNames[i]];
         if (data.name === name) {
           return i;
         }
@@ -287,9 +308,8 @@ define([
     toUrl: function() {
       var val = this.value();
       if (val) {
-        var index = parseInt(val, 10);
-        var node = this.anchorNodes[index];
-        var data = this.anchorData[index];
+        var data = this.anchorData[val];
+        var node = data.node;
         if (data.newAnchor) {
           node.innerHTML = '<a name="' + data.name + '" class="mce-item-anchor"></a>' + node.innerHTML;
         }
@@ -299,8 +319,7 @@ define([
     },
 
     set: function(val) {
-      var anchor = this.getIndex(val);
-      this.$select.select2('data', '' + anchor);
+      this.$select.select2('data', val);
     }
   });
 
