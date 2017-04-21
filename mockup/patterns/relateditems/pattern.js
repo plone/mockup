ff2554274cc6332a405549641ec81ea135047737
Asko Soukka <asko.soukka@iki.fi>
2017-04-21 10:55:36 +0300

    Fix issue where 'Open folder' in relateditems was not translated

--- mockup/patterns/relateditems/pattern.js
+++ mockup/patterns/relateditems/pattern.js
@@ -176,7 +176,10 @@ define([
         template = self.options[tpl + 'Template'];
       }
       // let's give all the options possible to the template generation
-      var options = $.extend(true, {}, self.options, item, {'browsing': self.browsing});
+      var options = $.extend(true, {}, self.options, item, {
+        'browsing': self.browsing,
+        'open_folder': _t('Open folder')
+      });
       options._item = item;
       return _.template(template)(options);
     },
