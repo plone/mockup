d8b6d1129ec1c1346cc9a633c6a892b6584ae21a
Johannes Raggam <thetetet@gmail.com>
2019-02-05 20:07:17 +0100

    Add readme info for the navigation marker pattern.

--- mockup/patterns/navigationmarker/pattern.js
+++ mockup/patterns/navigationmarker/pattern.js
@@ -1,3 +1,14 @@
+/* Navigation marker pattern.
+ *
+ * This pattern adds ``inPath`` and ``current`` classes to the navigation to
+ * allow a different style on selected navigation items or navigation items
+ * which are in the current path.
+ *
+ * This is done in JavaScript, so that the navigation pattern can be cached
+ * for the whole site.
+ *
+ */
+
 define([
     'jquery',
     'pat-base'
