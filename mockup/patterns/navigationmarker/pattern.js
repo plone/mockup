a97ccbe2fca77dc878bf7d26dac5e1d432e6b40e
Peter Holzer <peter.holzer@agitator.com>
2019-02-09 16:41:08 +0100

    Removed condition for mobile devices, solved via css and not needed anymore.

--- mockup/patterns/navigationmarker/pattern.js
+++ mockup/patterns/navigationmarker/pattern.js
@@ -21,16 +21,15 @@ define([
 
         init: function() {
             var href = document.querySelector('head link[rel="canonical"]').href || window.location.href;
+
             $('a', this.$el).each(function () {
                 if (href.indexOf(this.href) !== -1) {
                     var parent = $(this).parent();
 
-                    if ($('button.plone-navbar-toggle').is(':visible')) {  // only check checkboxes if it's a small device
-                        // check the input-openers within the path
-                        var check = parent.find('> input');
-                        if (check.length) {
-                            check[0].checked = true;
-                        }
+                    // check the input-openers within the path
+                    var check = parent.find('> input');
+                    if (check.length) {
+                        check[0].checked = true;
                     }
 
                     // set "inPath" to all nav items which are within the current path
@@ -42,8 +41,8 @@ define([
                     }
                 }
             });
-        }
 
+        }
     });
 
     return Navigation;
