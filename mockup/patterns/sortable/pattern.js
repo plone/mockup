b8ac50f1305a5fc11d4ef358348888dbcd015432
Johannes Raggam <thetetet@gmail.com>
2020-08-26 22:36:28 +0200

    Auto-reformat

--- mockup/patterns/sortable/pattern.js
+++ mockup/patterns/sortable/pattern.js
@@ -44,26 +44,26 @@
  */
 
 define([
-  'jquery',
-  'pat-base',
-  'jquery.event.drop',
-  'jquery.event.drag',
-], function($, Base, drop) {
-  'use strict';
+  "jquery",
+  "pat-base",
+  "jquery.event.drop",
+  "jquery.event.drag",
+], function ($, Base, drop) {
+  "use strict";
 
   var SortablePattern = Base.extend({
-    name: 'sortable',
-    trigger: '.pat-sortable',
-    parser: 'mockup',
+    name: "sortable",
+    trigger: ".pat-sortable",
+    parser: "mockup",
     defaults: {
-      selector: 'li',
-      dragClass: 'item-dragging',
-      cloneClass: 'dragging',
-      createDragItem: function(pattern, $el) {
+      selector: "li",
+      dragClass: "item-dragging",
+      cloneClass: "dragging",
+      createDragItem: function (pattern, $el) {
         return $el
           .clone()
           .addClass(pattern.options.cloneClass)
-          .css({ opacity: 0.75, position: 'absolute' })
+          .css({ opacity: 0.75, position: "absolute" })
           .appendTo(document.body);
       },
       drop: undefined, // callback function or name of global function
@@ -71,18 +71,18 @@ define([
         distance: 2,
       },
     },
-    init: function() {
+    init: function () {
       var self = this;
       var start = 0;
 
       self.$el
         .find(self.options.selector)
-        .drag('start', function(e, dd) {
+        .drag("start", function (e, dd) {
           var dragged = this;
           var $el = $(this);
           $(dragged).addClass(self.options.dragClass);
           drop({
-            tolerance: function(event, proxy, target) {
+            tolerance: function (event, proxy, target) {
               if ($(target.elem).closest(self.$el).length === 0) {
                 /* prevent dragging conflict over another drag area */
                 return;
@@ -90,8 +90,8 @@ define([
               var test = event.pageY > target.top + target.height / 2;
               $.data(
                 target.elem,
-                'drop+reorder',
-                test ? 'insertAfter' : 'insertBefore'
+                "drop+reorder",
+                test ? "insertAfter" : "insertBefore"
               );
               return this.contains(target, [event.pageX, event.pageY]);
             },
@@ -100,14 +100,14 @@ define([
           start = $el.index();
           return self.options.createDragItem(self, $el);
         })
-        .drag(function(e, dd) {
+        .drag(function (e, dd) {
           /*jshint eqeqeq:false */
           $(dd.proxy).css({
             top: dd.offsetY,
             left: dd.offsetX,
           });
           var drop = dd.drop[0],
-            method = $.data(drop || {}, 'drop+reorder');
+            method = $.data(drop || {}, "drop+reorder");
           /* XXX Cannot use triple equals here */
           if (method && drop && (drop != dd.current || method != dd.method)) {
             $(this)[method](drop);
@@ -116,19 +116,19 @@ define([
             dd.update();
           }
         }, self.options.dragOptions)
-        .drag('end', function(e, dd) {
+        .drag("end", function (e, dd) {
           var $el = $(this);
           $el.removeClass(self.options.dragClass);
           $(dd.proxy).remove();
           if (self.options.drop) {
-            if (typeof self.options.drop === 'string') {
+            if (typeof self.options.drop === "string") {
               window[self.options.drop]($el, $el.index() - start);
             } else {
               self.options.drop($el, $el.index() - start);
             }
           }
         })
-        .drop('init', function(e, dd) {
+        .drop("init", function (e, dd) {
           /*jshint eqeqeq:false */
           /* XXX Cannot use triple equals here */
           return this == dd.drag ? false : true;
