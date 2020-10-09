72c6c355b63c323ac35238f2173947474fb3ebec
Johannes Raggam <thetetet@gmail.com>
2020-10-10 01:13:30 +0200

    Update for es6/webpack structure

---
+++ src/setupTests.js
@@ -0,0 +1,25 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom/extend-expect";
+
+// need this for async/await in tests
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import jquery from "jquery";
+window.jQuery = jquery;
+jquery.expr.pseudos.visible = function () {
+    // Fix jQuery ":visible" selector always returns false in JSDOM.
+    // https://github.com/jsdom/jsdom/issues/1048#issuecomment-401599392
+    return true;
+};
+
+// pat-subform
+// See https://github.com/jsdom/jsdom/issues/1937#issuecomment-461810980
+window.HTMLFormElement.prototype.submit = () => {};
+
+// Do not output error messages
+import logging from "./core/logging";
+logging.setLevel(50); // level: FATAL
