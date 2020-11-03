98675cbbda61e65d6fdd48a1f26baaf0aa2b4e7c
Fred van Dijk <fredvd@gmail.com>
2020-11-03 21:57:23 +0100

    Don't depend on #edit-bar being present to update the toolbar after folder_contents navigation. Projects using plonetheme.barceloneta's backend.xml might not have that div/id in their html because of an error in the rules.xml

--- mockup/patterns/toolbar/pattern.js
+++ mockup/patterns/toolbar/pattern.js
@@ -517,7 +517,7 @@ define([
             var $newel = $(utils.parseBodyTag(data));
             var hasedit = $newel.find('#edit-zone').length;
             $newel = hasedit ? $newel.find('#edit-zone') : $newel;
-            var $replacetoolbar = $('#edit-bar').find('#edit-zone');
+            var $replacetoolbar = $('body').find('#edit-zone');
             $replacetoolbar.replaceWith($newel);
             Registry.scan($newel);
           });
