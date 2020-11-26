6ef999068f51a8029da45beb63f3dd92ed76c8e9
Johannes Raggam <thetetet@gmail.com>
2020-11-26 13:18:41 +0100

    core i18n + i18n-wrapper

--- src/core/i18n-wrapper.js
+++ src/core/i18n-wrapper.js
@@ -7,19 +7,19 @@
  *  Or, it'll default to "/plonejsi18n"
  */
 
-define(["mockup-i18n"], function (I18N) {
-    "use strict";
+import I18N from "./i18n";
 
-    // we're creating a singleton here so we can potentially
-    // delay the initialization of the translate catalog
-    // until after the dom is available
-    var _t = null;
-    return function (msgid, keywords) {
-        if (_t === null) {
-            var i18n = new I18N();
-            i18n.loadCatalog("widgets");
-            _t = i18n.MessageFactory("widgets");
-        }
-        return _t(msgid, keywords);
-    };
-});
+// we're creating a singleton here so we can potentially
+// delay the initialization of the translate catalog
+// until after the dom is available
+let _t = null;
+const translate = function (msgid, keywords) {
+    if (_t === null) {
+        var i18n = new I18N();
+        i18n.loadCatalog("widgets");
+        _t = i18n.MessageFactory("widgets");
+    }
+    return _t(msgid, keywords);
+};
+
+export default translate;
