b0e9edc55c72bc1d882e660157c467042116d82f
Johannes Raggam <thetetet@gmail.com>
2021-01-08 15:25:50 +0100

    pat formautofocus

---
+++ src/pat/formautofocus/README.md
@@ -0,0 +1,16 @@
+---
+permalink: "pat/formautofocus/"
+title: Formautofocus
+---
+
+# Formautofocus pattern.
+
+
+## Configuration
+
+| Option | Type | Default | Description |
+|:-:|:-:|:-:|:-:|
+| condition | string | "div.error" | |
+| target | string | "div.error :input:not(.formTabs):visible:first" |  |
+| always | string | ":input:not(.formTabs):visible:first" |  |
+
---
+++ src/pat/formautofocus/formautofocus.js
@@ -0,0 +1,20 @@
+import $ from "jquery";
+import Base from "patternslib/src/core/base";
+
+export default Base.extend({
+    name: "formautofocus",
+    trigger: ".pat-formautofocus",
+    parser: "mockup",
+    defaults: {
+        condition: "div.error",
+        target: "div.error :input:not(.formTabs):visible:first",
+        always: ":input:not(.formTabs):visible:first",
+    },
+    init() {
+        if ($(this.options.condition, this.$el).length !== 0) {
+            $(this.options.target, this.$el).focus();
+        } else {
+            $(this.options.always, this.$el).focus();
+        }
+    },
+});
