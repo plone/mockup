27ce51ed0bbf13f92ada79c6758286e0a65494c7
Johannes Raggam <thetetet@gmail.com>
2021-01-08 15:25:35 +0100

    pat formunloadalert

---
+++ src/pat/formunloadalert/README.md
@@ -0,0 +1,49 @@
+---
+permalink: "pat/formunloadalert/"
+title: Formunloadalert
+---
+
+# Formunloadalert pattern.
+
+PATTERN description
+
+## Configuration
+
+| Option | Type | Default | Description |
+|:-:|:-:|:-:|:-:|
+| changingEvents | string | "change keyup paste" | Events on which to check for changes (space-separated). |
+| changingFields | string | "input,select,textarea,fileupload" | Fields on which to check for changes (comma-separated). |
+| message | string  | "Discard changes? If you click OK, any changes you have made will be lost." | Confirmation message to display when dirty form is being unloaded. |
+
+
+## Examples
+
+### Example 1
+
+<form class="pat-formunloadalert" onsubmit="javascript:return false;">
+  <input type="text" value="" />
+  <select>
+    <option value="1">value 1</option>
+    <option value="2">value 2</option>
+  </select>
+  <input
+    class="btn btn-large btn-primary"
+    type="submit" value="Submit" />
+  <br />
+  <a href="/">Click here to go somewhere else</a>
+</form>
+
+```html
+<form class="pat-formunloadalert" onsubmit="javascript:return false;">
+  <input type="text" value="" />
+  <select>
+    <option value="1">value 1</option>
+    <option value="2">value 2</option>
+  </select>
+  <input
+    class="btn btn-large btn-primary"
+    type="submit" value="Submit" />
+  <br />
+  <a href="/">Click here to go somewhere else</a>
+</form>
+```
---
+++ src/pat/formunloadalert/formunloadalert.js
@@ -0,0 +1,69 @@
+import $ from "jquery";
+import Base from "patternslib/src/core/base";
+import _t from "../../core/i18n-wrapper";
+
+export default Base.extend({
+    name: "formunloadalert",
+    trigger: ".pat-formunloadalert",
+    parser: "mockup",
+    _changed: false, // Stores a listing of raised changes by their key
+    _suppressed: false, // whether or not warning should be suppressed
+    defaults: {
+        message: _t(
+            "Discard changes? If you click OK, " +
+                "any changes you have made will be lost."
+        ),
+        // events on which to check for changes
+        changingEvents: "change keyup paste",
+        // fields on which to check for changes
+        changingFields: "input,select,textarea,fileupload",
+    },
+    init() {
+        var self = this;
+        // if this is not a form just return
+        if (!self.$el.is("form")) {
+            return;
+        }
+
+        $(self.options.changingFields, self.$el).on(
+            self.options.changingEvents,
+            (evt) => {
+                self._changed = true;
+            }
+        );
+
+        var $modal = self.$el.parents(".plone-modal");
+        if ($modal.length !== 0) {
+            $modal.data("patternPloneModal").on("hide", function (e) {
+                var modal = $modal.data("patternPloneModal");
+                if (modal) {
+                    modal._suppressHide = self._handleUnload.call(self, e);
+                }
+            });
+        } else {
+            $(window).on("beforeunload", (e) => {
+                return self._handleUnload(e);
+            });
+        }
+
+        self.$el.on("submit", (e) => {
+            self._suppressed = true;
+        });
+    },
+    _handleUnload(e) {
+        var self = this;
+        if (self._suppressed) {
+            self._suppressed = false;
+            return undefined;
+        }
+        if (self._changed) {
+            var msg = self.options.message;
+            self._handleMsg(e, msg);
+            $(window).trigger("messageset");
+            return msg;
+        }
+    },
+    _handleMsg(e, msg) {
+        (e || window.event).returnValue = msg;
+    },
+});
