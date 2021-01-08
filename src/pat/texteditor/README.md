ddd701e6f612947ca88c52fab3e0a2dcbb8c96c4
Johannes Raggam <thetetet@gmail.com>
2021-01-08 15:53:08 +0100

    pat texteditor

---
+++ src/pat/texteditor/README.md
@@ -0,0 +1,85 @@
+---
+permalink: "pat/texteditor/"
+title: Text Editor
+---
+
+# Text Editor pattern.
+
+## Configuration
+
+| Option | Type | Default | Description |
+|:-:|:-:|:-:|:-:|
+| theme | string | null | Theme to use with editor. defaults to whatever it ships with. |
+| mode | string | What type of syntax is it? | text |
+| width | integer | 500 | Width of the editor. |
+| height | integer | 200 | Height of the editor. |
+| tabSize | integer | 4 | TODO |
+| softTabs | boolean | true | Use spaces for tabs. |
+| wrapMode | boolean | false | Wrap text. |
+| showGutter | boolean | TODO | true |
+| showPrintMargin | boolean | Show print margin. | false |
+| readOnly | boolean | Read only editor. | false |
+
+
+## Examples
+
+### Default
+
+<pre class="pat-texteditor" data-pat-texteditor="theme:clouds">
+foobar
+</pre>
+
+```html
+<pre class="pat-texteditor" data-pat-texteditor="theme:clouds">
+foobar
+</pre>
+```
+
+
+### Different theme
+
+<pre class="pat-texteditor" data-pat-texteditor="mode:javascript;theme:dawn;">
+var foo = 'bar';
+function foobar() {
+ return foo;
+}
+</pre>
+
+```html
+<pre class="pat-texteditor" data-pat-texteditor="mode:javascript;theme:dawn;">
+var foo = 'bar';
+function foobar() {
+ return foo;
+}
+</pre>
+```
+
+
+### Different options
+
+<pre class="pat-texteditor"
+    data-pat-texteditor="mode:javascript;
+                  theme:ambiance;
+                  tabSize:2;
+                  showGutter:false;
+                  showPrintMargin:true;">
+var foo = 'bar';
+function foobar() {
+ return foo;
+}
+</pre>
+
+```html
+<pre class="pat-texteditor"
+    data-pat-texteditor="mode:javascript;
+                  theme:ambiance;
+                  tabSize:2;
+                  showGutter:false;
+                  showPrintMargin:true;">
+var foo = 'bar';
+function foobar() {
+ return foo;
+}
+</pre>
+```
+
---
+++ src/pat/texteditor/texteditor.js
@@ -0,0 +1,106 @@
+import $ from "jquery";
+import Base from "patternslib/src/core/base";
+import utils from "../../core/utils";
+import "ace-builds/src/ace";
+
+export default Base.extend({
+    name: "texteditor",
+    trigger: ".pat-texteditor",
+    parser: "mockup",
+    defaults: {
+        theme: null,
+        mode: "text",
+        width: 500,
+        height: 200,
+        tabSize: 4,
+        softTabs: true,
+        wrapMode: false,
+        showGutter: true,
+        showPrintMargin: false,
+        readOnly: false,
+    },
+    init: function () {
+        var self = this;
+        if (!window.ace) {
+            // XXX hack...
+            // wait, try loading later
+            setTimeout(function () {
+                self.init();
+            }, 200);
+            return;
+        }
+        var ace = window.ace;
+
+        ace.config.set("packaged", true);
+        ace.config.set(
+            "basePath",
+            "++plone++static/components/ace-builds/src/"
+        );
+
+        // set id on current element
+        var id = utils.setId(self.$el);
+        self.$wrapper = $('<div class="editorWrapper" />').css({
+            height: parseInt(self.options.height) + 25, // weird sizing issue here...
+            width: self.options.width,
+            position: "relative",
+        });
+        if (!self.$el.parent().hasClass("editorWrapper")) {
+            self.$el.wrap(self.$wrapper);
+        }
+        self.$el.css({
+            width: self.options.width,
+            height: self.options.height,
+            position: "absolute",
+        });
+
+        self.editor = ace.edit(id);
+        if (self.options.theme) {
+            self.setTheme(self.options.theme);
+        }
+        self.editor.getSession().setMode("ace/mode/" + self.options.mode);
+        self.editor.getSession().setTabSize(parseInt(self.options.tabSize, 10));
+        self.editor
+            .getSession()
+            .setUseSoftTabs(utils.bool(self.options.softTabs));
+        self.editor
+            .getSession()
+            .setUseWrapMode(utils.bool(self.options.wrapMode));
+        self.editor.renderer.setShowGutter(utils.bool(self.options.showGutter));
+        self.editor.setShowPrintMargin(
+            utils.bool(self.options.showPrintMargin)
+        );
+        self.editor.setReadOnly(utils.bool(self.options.readOnly));
+    },
+    setSyntax: function (name) {
+        var self = this;
+        var modes = {
+            js: "javascript",
+            txt: "text",
+            css: "css",
+            html: "html",
+            xml: "xml",
+            less: "less",
+            py: "python",
+            pt: "xml",
+            cfg: "ini",
+        };
+
+        var extension = name.substr(name.lastIndexOf(".") + 1);
+        var mode = modes[extension];
+
+        if (mode !== undefined) {
+            self.editor.getSession().setMode("ace/mode/" + mode);
+            return true;
+        }
+    },
+    setTheme: function (theme) {
+        var self = this;
+        self.editor.setTheme("ace/theme/" + theme);
+    },
+    setText: function (data) {
+        var self = this;
+        if (self.editor) {
+            self.editor.setValue(data);
+        }
+    },
+});
