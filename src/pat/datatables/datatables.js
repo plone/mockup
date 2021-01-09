8a1508545c4bda7083216ca1fa212c44c6405a1f
Johannes Raggam <thetetet@gmail.com>
2021-01-09 16:17:45 +0100

    pat datatables

--- src/pat/datatables/datatables.js
+++ src/pat/datatables/datatables.js
@@ -16,16 +16,21 @@ export default Base.extend({
         await import("datatables.net");
         await import("datatables.net-bs");
         await import("datatables.net-buttons");
+        await import("datatables.net-buttons-bs");
         await import("datatables.net-buttons/js/buttons.colVis");
         await import("datatables.net-buttons/js/buttons.html5");
         await import("datatables.net-buttons/js/buttons.print");
-        await import("datatables.net-buttons-bs");
         await import("datatables.net-colreorder");
-        await import("datatables.net-rowreorder");
+        await import("datatables.net-colreorder-bs");
         await import("datatables.net-fixedcolumns");
+        await import("datatables.net-fixedcolumns-bs");
         await import("datatables.net-fixedheader");
+        await import("datatables.net-fixedheader-bs");
+        await import("datatables.net-rowreorder");
+        await import("datatables.net-rowreorder-bs");
         await import("datatables.net-select");
+        await import("datatables.net-select-bs");
 
-        this.$el.DataTable(this.options);
+        $(this.el).DataTable(this.options);
     },
 });
