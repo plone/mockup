925bceca00b03bff1bb804853dbe0ab585634f6e
Johannes Raggam <thetetet@gmail.com>
2021-02-10 16:50:57 +0100

    Plone integration - fallback to default Plone static url.

--- src/public_path.js
+++ src/public_path.js
@@ -1,3 +1,3 @@
 // NOTE: Import this file before any other files
 // Overwrite path to load resources or use default one.
-__webpack_public_path__ = window.__patternslib_public_path__ || "/dist/"; // eslint-disable-line no-undef
+__webpack_public_path__ = window.__patternslib_public_path__ || window.PORTAL_URL + "/++plone++static/" || "/dist/"; // eslint-disable-line no-undef
