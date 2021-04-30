a991f2c3627ceb54c2d0fd2a9d55151229ff3081
Johannes Raggam <thetetet@gmail.com>
2021-04-30 16:36:22 +0200

    PLIP 3211 - Add missing regenerator-runtime improt for async calls.

--- src/pat/relateditems/relateditems.js
+++ src/pat/relateditems/relateditems.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime/runtime"; // needed for ``await`` support
 import $ from "jquery";
 import _ from "underscore";
 import Base from "@patternslib/patternslib/src/core/base";
