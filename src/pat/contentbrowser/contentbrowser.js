18c1ff19057a52b82414d1cd00047f134b98a8be
Johannes Raggam <thetetet@gmail.com>
2021-05-23 15:40:36 +0200

    Towards integrating the content browser.

--- src/pat/contentbrowser/contentbrowser.js
+++ src/pat/contentbrowser/contentbrowser.js
@@ -7,8 +7,104 @@ export default Base.extend({
     parser: "mockup",
 
     async init() {
-        let contentbrowser = await import("@plone/volto/components/manage/Contents/Contents.jsx"); // prettier-ignore
+        window.__CLIENT__ = true; // Define as volto client
 
-        this.el.style["background-color"] = "green";
+        const React = (await import("react")).default;
+        const ReactDOM = (await import("react-dom")).default;
+        const IntlProvider = (await import("react-intl")).IntlProvider;
+        const Provider = (await import("react-redux")).Provider;
+        const ContentBrowser = (await import("@plone/volto/components/manage/Contents/Contents.jsx")).default; // prettier-ignore
+        const MemoryRouter = (await import("react-router-dom")).MemoryRouter;
+        const configureStore = (await import("redux-mock-store")).default;
+        const mockStore = configureStore();
+
+        const store = mockStore({
+            actions: {
+                actions: {
+                    document_actions: [],
+                    object: [
+                        {
+                            icon: "",
+                            id: "folderContents",
+                            title: "Contents",
+                        },
+                    ],
+                },
+            },
+            userSession: {
+                token: "14134234123qwdaf",
+            },
+            search: {
+                items: [
+                    {
+                        "@id": "/blog",
+                        "@type": "Folder",
+                        "title": "Blog",
+                        "descripton": "My Blog",
+                        "ModificationDate": "2017-04-19T22:48:56+02:00",
+                        "EffectiveDate": "2017-04-19T22:48:56+02:00",
+                        "review_state": "private",
+                    },
+                ],
+                total: 1,
+            },
+            breadcrumbs: {
+                items: [
+                    {
+                        url: "/blog",
+                        title: "Blog",
+                    },
+                ],
+            },
+            clipboard: {
+                action: "copy",
+                source: ["/blog"],
+                request: {
+                    loading: false,
+                    loaded: false,
+                },
+            },
+            content: {
+                delete: {
+                    loading: false,
+                    loaded: false,
+                },
+                update: {
+                    loading: false,
+                    loaded: false,
+                },
+                updatecolumns: {
+                    loading: false,
+                    loaded: false,
+                },
+            },
+            intl: {
+                locale: "en",
+                messages: {},
+            },
+        });
+
+        ReactDOM.render(
+            React.createElement(
+                Provider,
+                { store: store },
+                React.createElement(
+                    MemoryRouter,
+                    {},
+                    React.createElement(
+                        IntlProvider,
+                        { locale: "en" },
+                        React.createElement(
+                            ContentBrowser,
+                            {
+                                location: { pathname: "/blog" },
+                            },
+                            null
+                        )
+                    )
+                )
+            ),
+            this.el
+        );
     },
 });
