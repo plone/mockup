e0de1dc29d3f47bb1828ecadaa1aab194dfc5d32
Thokas <github@thokas.de>
2021-08-18 12:08:19 +0200

    bump datatables.net versions and use bootstrap5 stylings

--- src/pat/datatables/datatables.js
+++ src/pat/datatables/datatables.js
@@ -13,31 +13,31 @@ export default Base.extend({
     },
 
     async init() {
-        import("datatables.net-bs/css/dataTables.bootstrap.css");
-        import("datatables.net-buttons-bs/css/buttons.bootstrap.min.css");
-        import("datatables.net-colreorder-bs/css/colReorder.bootstrap.min.css");
-        import("datatables.net-rowreorder-bs/css/rowReorder.bootstrap.min.css");
-        import("datatables.net-select-bs/css/select.bootstrap.min.css");
-        import("datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css"); // prettier-ignore
-        import("datatables.net-fixedcolumns-bs/css/fixedColumns.bootstrap.min.css"); // prettier-ignore
+        import("datatables.net-bs5/css/dataTables.bootstrap5.min.css");
+        import("datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css");
+        import("datatables.net-colreorder-bs5/css/colReorder.bootstrap5.min.css");
+        import("datatables.net-rowreorder-bs5/css/rowReorder.bootstrap5.min.css");
+        import("datatables.net-select-bs5/css/select.bootstrap5.min.css");
+        import("datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.min.css"); // prettier-ignore
+        import("datatables.net-fixedcolumns-bs5/css/fixedColumns.bootstrap5.min.css"); // prettier-ignore
 
         await import("datatables.net");
-        await import("datatables.net-bs");
+        await import("datatables.net-bs5");
         await import("datatables.net-buttons");
-        await import("datatables.net-buttons-bs");
+        await import("datatables.net-buttons-bs5");
         await import("datatables.net-buttons/js/buttons.colVis");
         await import("datatables.net-buttons/js/buttons.html5");
         await import("datatables.net-buttons/js/buttons.print");
         await import("datatables.net-colreorder");
-        await import("datatables.net-colreorder-bs");
+        await import("datatables.net-colreorder-bs5");
         await import("datatables.net-fixedcolumns");
-        await import("datatables.net-fixedcolumns-bs");
+        await import("datatables.net-fixedcolumns-bs5");
         await import("datatables.net-fixedheader");
-        await import("datatables.net-fixedheader-bs");
+        await import("datatables.net-fixedheader-bs5");
         await import("datatables.net-rowreorder");
-        await import("datatables.net-rowreorder-bs");
+        await import("datatables.net-rowreorder-bs5");
         await import("datatables.net-select");
-        await import("datatables.net-select-bs");
+        await import("datatables.net-select-bs5");
 
         $(this.el).DataTable(this.options);
     },
