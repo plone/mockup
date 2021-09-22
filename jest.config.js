6f244c3c2e7da9088cb6fddb0668be05b528dac0
Johannes Raggam <thetetet@gmail.com>
2021-09-22 18:32:50 +0200

    Use test environment 'jsdom'

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,6 @@ module.exports = {
     moduleNameMapper: {
         "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     },
+    testEnvironment: "jsdom",
     transformIgnorePatterns: ["/node_modules/(?!.*patternslib/*).+\\.[t|j]sx?$"],
 };
