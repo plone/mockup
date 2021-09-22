f57f8867433c39fead03d78c9f1f58479b9a5d41
Johannes Raggam <thetetet@gmail.com>
2021-09-22 12:30:39 +0200

    Align with build changes from Patternslib v5.

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,16 @@ const patternslib_config = require("@patternslib/patternslib/webpack/webpack.con
 const webpack = require("webpack");
 
 module.exports = async (env, argv, build_dirname = __dirname) => {
-    const config = patternslib_config(env, argv);
-
-    config.entry = {
-        "bundle": path.resolve(build_dirname, "src/patterns.js"),
-        "bundle-polyfills": path.resolve(build_dirname, "node_modules/@patternslib/patternslib/src/polyfills.js"), // prettier-ignore
+    let config = {
+        entry: {
+            "bundle": path.resolve(build_dirname, "src/patterns.js"),
+            "bundle-polyfills": path.resolve(build_dirname, "node_modules/@patternslib/patternslib/src/polyfills.js"), // prettier-ignore
+        },
     };
-    config.output.path = path.resolve(build_dirname, "dist/");
 
-    // Correct moment alias
-    config.resolve.alias.moment = path.resolve(build_dirname, "node_modules/moment"); // prettier-ignore
+    config = patternslib_config(env, argv, config);
+
+    config.output.path = path.resolve(build_dirname, "dist/");
 
     config.plugins.push(
         new webpack.ProvidePlugin({
@@ -32,8 +32,8 @@ module.exports = async (env, argv, build_dirname = __dirname) => {
     config.resolve.extensions = [".wasm", ".mjs", ".js", ".json", ".svelte"];
     config.resolve.mainFields = ["svelte", "browser", "module", "main"];
 
-        // Set public path to override __webpack_public_path__
-        // for webpack-dev-server
+    if (process.env.NODE_ENV === "development") {
+        // Set public path to override __webpack_public_path__ for webpack-dev-server
         config.output.publicPath = "/dist/";
         config.devServer = {
             inline: true,
@@ -44,12 +44,6 @@ module.exports = async (env, argv, build_dirname = __dirname) => {
                 ignored: ["node_modules/**", "mockup/**", "docs/**"],
             },
         };
-    }
-
-    if (argv.mode === "production") {
-        // Also create minified bundles along with the non-minified ones.
-        config.entry["bundle.min"] = config.entry["bundle"];
-        config.entry["bundle-polyfills.min"] = config.entry["bundle-polyfills"];
     }
 
     if (env && env.DEPLOYMENT === "plone") {
