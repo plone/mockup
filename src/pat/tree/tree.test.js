54f33e74a9bac559e1523f0abc9aae36959456b8
Johannes Raggam <thetetet@gmail.com>
2021-09-22 18:39:30 +0200

    pat tree: test fixes

--- src/pat/tree/tree.test.js
+++ src/pat/tree/tree.test.js
@@ -1,17 +1,14 @@
 import $ from "jquery";
 import Pattern from "./tree";
-import utils from "../../core/utils";
+import utils from "@patternslib/patternslib/src/core/utils";
 
 $.fx.off = true;
 
 describe("Tree", function () {
-    it("loads the tree with data", function () {
-        document.body.innerHTML = `
-            <div class="pat-tree"></div>
-        `;
-        //await utils.timeout(1);
+    it("loads the tree with data", async function () {
+        document.body.innerHTML = `<div class="pat-tree"/>`;
         const el = document.querySelector(".pat-tree");
-        var tree = new Pattern(el, {
+        new Pattern(el, {
             autoOpen: true,
             data: [
                 {
@@ -31,25 +28,28 @@ describe("Tree", function () {
                 },
             ],
         });
+        await utils.timeout(1);
         expect(document.querySelectorAll("ul").length).toEqual(3);
     });
-    it("load string of json", function () {
-        document.body.innerHTML = `
-            <div class="pat-tree"/>
-        `;
+    it("load string of json", async function () {
+        document.body.innerHTML = `<div class="pat-tree"/>`;
         const el = document.querySelector(".pat-tree");
-        var tree = new Pattern(el, {
+        new Pattern(el, {
             autoOpen: true,
-            data:
-                "[" +
-                '{"label": "node1",' +
-                '"children": [{' +
-                '"label": "child1"' +
-                "},{" +
-                '"label": "child2"' +
-                "}]" +
-                "}]",
+            data: `[
+                {
+                    "label": "node1",
+                    "children": [
+                        {
+                            "label": "child1"
+                        },{
+                            "label": "child2"
+                        }
+                    ]
+                }
+            ]`,
         });
+        await utils.timeout(1);
         expect(document.querySelectorAll("ul").length).toEqual(2);
     });
 });
