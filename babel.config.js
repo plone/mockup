672422068639600dd96290fa06272b56225324b3
Johannes Raggam <thetetet@gmail.com>
2021-09-22 12:24:27 +0200

    babel: transpile less in development mode.

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,35 @@
-module.exports = {
-    presets: [
-        [
-            "@babel/preset-env",
-            {
-                useBuiltIns: "entry",
-                corejs: 3,
-            },
+module.exports = (env) => {
+    let config = {
+        presets: [
+            [
+                "@babel/preset-env",
+                {
+                    useBuiltIns: "entry",
+                    corejs: 3,
+                },
+            ],
         ],
-    ],
-    plugins: ["@babel/plugin-proposal-optional-chaining"],
+        plugins: ["@babel/plugin-proposal-optional-chaining"],
+    };
+
+    // passed via NODE_ENV=development environment variable.
+    if (env.env() === "development") {
+        // For development, do less transformations for better readability.
+        config = {
+            presets: [
+                [
+                    "@babel/preset-env",
+                    {
+                        modules: false,
+                        debug: true,
+                        useBuiltIns: false,
+                        targets: "last 1 Chrome version, last 1 Firefox version",
+                    },
+                ],
+            ],
+            plugins: ["@babel/plugin-proposal-optional-chaining"],
+        };
+    }
+
+    return config;
 };
