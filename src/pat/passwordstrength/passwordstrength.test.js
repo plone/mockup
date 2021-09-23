379ea36c7d13fc8c61756f7965710b30ee00e582
Yuri Carrer <yuri.carrer@unipd.it>
2021-09-23 17:13:23 +0200

    With the help of @MrTango, fixed syntax on tests (still 3 tests to be fixed)

--- src/pat/passwordstrength/passwordstrength.test.js
+++ src/pat/passwordstrength/passwordstrength.test.js
@@ -1,128 +1,124 @@
-define([
-    "expect",
-    "jquery",
-    "pat-registry",
-    "mockup-patterns-passwordstrength",
-], function (expect, $, registry, PasswordStrength) {
-    "use strict";
+import "./passwordstrength";
+import $ from "jquery";
+import registry from "@patternslib/patternslib/src/core/registry";
+import utils from "@patternslib/patternslib/src/core/utils";
 
-    window.mocha.setup("bdd");
-    $.fx.off = true;
+// window.mocha.setup("bdd");
+// $.fx.off = true;
 
-    /* ==========================
-   TEST: Password strength
-   ========================== */
+/* ==========================
+TEST: Password strength
+========================== */
 
-    describe("Password strength", function () {
-        var orig_setTimeout = window.setTimeout;
+describe("Password strength", function () {
+var orig_setTimeout = window.setTimeout;
 
-        function fakeZxcvbn(input, strings) {
-            window.providedStrings = strings;
-            return {
-                score: Math.min(input.length, 4),
-            };
-        }
+function fakeZxcvbn(input, strings) {
+    window.providedStrings = strings;
+    return {
+	score: Math.min(input.length, 4),
+    };
+}
 
-        function getScripts() {
-            return [].map
-                .call(document.getElementsByTagName("script"), function (s) {
-                    return s.src;
-                })
-                .filter(function (src) {
-                    return src.indexOf("zxcvbn") !== -1;
-                });
-        }
+function getScripts() {
+    return [].map
+	.call(document.getElementsByTagName("script"), function (s) {
+	    return s.src;
+	})
+	.filter(function (src) {
+	    return src.indexOf("zxcvbn") !== -1;
+	});
+}
 
-        beforeEach(function () {
-            window.setTimeout = function (fun, interval) {
-                window.providedInterval = interval;
-                fun();
-                return 0;
-            };
-        });
+beforeEach(function () {
+    window.setTimeout = function (fun, interval) {
+	window.providedInterval = interval;
+	fun();
+	return 0;
+    };
+});
 
-        afterEach(function () {
-            window.setTimeout = orig_setTimeout;
-        });
+afterEach(function () {
+    window.setTimeout = orig_setTimeout;
+});
 
-        it("adds markup below input element", function () {
-            var $el = $(
-                '<div><input type="password" class="pat-passwordstrength" /></div>'
-            );
-            window.zxcvbn = fakeZxcvbn;
-            registry.scan($el);
+it("adds markup below input element", function () {
+    var $el = $(
+	'<div><input type="password" class="pat-passwordstrength" /></div>'
+    );
+    window.zxcvbn = fakeZxcvbn;
+    registry.scan($el);
 
-            expect($el.find(".progress").length).to.be.equal(1);
-            expect($el.find(".progress-bar").length).to.be.equal(1);
-        });
+    expect($el.find(".progress").length).toEqual(1);
+    expect($el.find(".progress-bar").length).toEqual(1);
+});
 
-        it("tries to load zxcvbn once if not available", function () {
-            var $el2,
-                $el = $(
-                    '<div><input type="password" class="pat-passwordstrength" data-pat-passwordstrength="zxcvbn: http://example.com/zxcvbn.js" /></div>'
-                );
-            window.zxcvbn = undefined;
-            expect(getScripts().length).to.equal(0);
-            registry.scan($el);
-            expect(getScripts().length).to.equal(1);
-            expect(getScripts()).to.contain("http://example.com/zxcvbn.js");
+it("tries to load zxcvbn once if not available", function () {
+    var $el2,
+	$el = $(
+	    '<div><input type="password" class="pat-passwordstrength" data-pat-passwordstrength="zxcvbn: http://example.com/zxcvbn.js" /></div>'
+	);
+    window.zxcvbn = undefined;
+    expect(getScripts().length).toEqual(0);
+    registry.scan($el);
+    expect(getScripts().length).toEqual(1);
+    expect(getScripts()).toContain("http://example.com/zxcvbn.js");
 
-            $el.find("input[type=password]").attr("value", "a").trigger("keyup");
-            expect(getScripts().length).to.equal(1);
-            $el.find("input[type=password]").attr("value", "aa").trigger("keyup");
-            expect(getScripts().length).to.equal(1);
-            $el.find("input[type=password]").attr("value", "aaa").trigger("keyup");
-            expect(getScripts().length).to.equal(1);
-            expect(getScripts()).to.contain("http://example.com/zxcvbn.js");
+    $el.find("input[type=password]").attr("value", "a").trigger("keyup");
+    expect(getScripts().length).toEqual(1);
+    $el.find("input[type=password]").attr("value", "aa").trigger("keyup");
+    expect(getScripts().length).toEqual(1);
+    $el.find("input[type=password]").attr("value", "aaa").trigger("keyup");
+    expect(getScripts().length).toEqual(1);
+    expect(getScripts()).toContain("http://example.com/zxcvbn.js");
 
-            $el2 = $(
-                '<div><input type="password" class="pat-passwordstrength" data-pat-passwordstrength="zxcvbn: http://example.com/zxcvbn.js" /></div>'
-            );
-            registry.scan($el2);
-            expect(getScripts().length).to.equal(1);
-            expect(getScripts()).to.contain("http://example.com/zxcvbn.js");
-        });
+    $el2 = $(
+	'<div><input type="password" class="pat-passwordstrength" data-pat-passwordstrength="zxcvbn: http://example.com/zxcvbn.js" /></div>'
+    );
+    registry.scan($el2);
+    expect(getScripts().length).toEqual(1);
+    expect(getScripts()).toContain("http://example.com/zxcvbn.js");
+});
 
-        it("sets level based on the entered password", function () {
-            var $el = $(
-                '<div><input type="password" class="pat-passwordstrength" /></div>'
-            );
-            window.zxcvbn = fakeZxcvbn;
-            registry.scan($el);
+it("sets level based on the entered password", function () {
+    var $el = $(
+	'<div><input type="password" class="pat-passwordstrength" /></div>'
+    );
+    window.zxcvbn = fakeZxcvbn;
+    registry.scan($el);
 
-            $el.find("input[type=password]").attr("value", "a").trigger("keyup");
-            expect($el.find(".progress-bar").attr("class")).to.equal(
-                "progress-bar w-25 bg-danger"
-            );
-            expect(window.providedStrings.length).to.equal(0);
-            expect(window.providedInterval).to.equal(500);
+    $el.find("input[type=password]").attr("value", "a").trigger("keyup");
+    expect($el.find(".progress-bar").attr("class")).toEqual(
+	"progress-bar w-25 bg-danger"
+    );
+    expect(window.providedStrings.length).toEqual(0);
+    expect(window.providedInterval).toEqual(500);
 
-            $el.find("input[type=password]").attr("value", "aA1!bB$").trigger("keyup");
-            expect($el.find(".progress-bar").attr("class")).to.equal(
-                "progress-bar w-50 bg-warning"
-            );
-            expect(window.providedStrings.length).to.equal(0);
-            expect(window.providedInterval).to.equal(500);
-        });
+    $el.find("input[type=password]").attr("value", "aA1!bB$").trigger("keyup");
+    expect($el.find(".progress-bar").attr("class")).toEqual(
+	"progress-bar w-50 bg-warning"
+    );
+    expect(window.providedStrings.length).toEqual(0);
+    expect(window.providedInterval).toEqual(500);
+});
 
-        it("provides zxcvbn with other form field values", function () {
-            var $el = $(
-                "<form>" +
-                    '<input type="input" name="username" value="bob_geldof" />' +
-                    '<input type="password" class="pat-passwordstrength" />' +
-                    '<input type="checkbox" name="spam_me" checked="yes">' +
-                    "</form>"
-            );
-            window.zxcvbn = fakeZxcvbn;
-            registry.scan($el);
+it("provides zxcvbn with other form field values", function () {
+    var $el = $(
+	"<form>" +
+	    '<input type="input" name="username" value="bob_geldof" />' +
+	    '<input type="password" class="pat-passwordstrength" />' +
+	    '<input type="checkbox" name="spam_me" checked="yes">' +
+	    "</form>"
+    );
+    window.zxcvbn = fakeZxcvbn;
+    registry.scan($el);
 
-            $el.find("input[type=password]").attr("value", "a").trigger("keyup");
-            expect($el.find(".progress-bar").attr("class")).to.equal(
-                "progress-bar w-0 bg-transparent"
-            );
-            expect(window.providedStrings.length).to.equal(2);
-            expect(window.providedStrings).to.contain("bob_geldof");
-            expect(window.providedStrings).to.contain("on");
-        });
-    });
+    $el.find("input[type=password]").attr("value", "a").trigger("keyup");
+    expect($el.find(".progress-bar").attr("class")).toEqual(
+	"progress-bar w-0 bg-transparent"
+    );
+    expect(window.providedStrings.length).toEqual(2);
+    expect(window.providedStrings).toContain("bob_geldof");
+    expect(window.providedStrings).toContain("on");
+});
 });
