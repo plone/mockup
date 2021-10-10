6ad694738b89514e3484c3ec1f1ad95d6daac9ee
Johannes Raggam <thetetet@gmail.com>
2021-10-10 22:47:09 +0200

    Fix pat-markspeciallinks tests. Includes changes to updated icons.

--- src/pat/markspeciallinks/markspeciallinks.test.js
+++ src/pat/markspeciallinks/markspeciallinks.test.js
@@ -1,93 +1,82 @@
 import "./markspeciallinks";
-import $ from "jquery";
 import registry from "@patternslib/patternslib/src/core/registry";
 import utils from "@patternslib/patternslib/src/core/utils";
 
-/* ==========================
-   TEST: MarkSpecialLinks
-  ========================== */
-
 describe("MarkSpecialLinks", function () {
-    beforeEach(function () {
-        this.$el = $(
-            "" +
-                '<div class="pat-markspeciallinks">' +
-                '  <p>Find out What&#39s new in <a href="http://www.plone.org">Plone</a>.<br>' +
-                '     Plone is written in <a class="link-plain" href="http://www.python.org">Python</a>.' +
-                "  </p>" +
-                "</div>" +
-                '<div class="pat-markspeciallinks" data-pat-markspeciallinks="external_links_open_new_window: true">' +
-                '  <p>Find out What&#39s new in <a href="http://www.plone.org">Plone</a>.<br>' +
-                '     Plone is written in <a class="link-plain" href="http://www.python.org">Python</a>.' +
-                "  </p>" +
-                "</div>" +
-                '<div class="pat-markspeciallinks" data-pat-markspeciallinks="mark_special_links: false">' +
-                '  <p>Find out What&#39s new in <a href="http://www.plone.org">Plone</a>.<br>' +
-                '     Plone is written in <a class="link-plain" href="http://www.python.org">Python</a>.' +
-                "  </p>" +
-                "</div>" +
-                '<div class="icons pat-markspeciallinks">' +
-                "    <ul>" +
-                '      <li><a href="http://www.plone.org">http</a></li>' +
-                '      <li><a href="https://www.plone.org">https</a></li>' +
-                '      <li><a href="mailto:info@plone.org">mailto</a></li>' +
-                '      <li><a href="ftp://www.plone.org">ftp</a></li>' +
-                '      <li><a href="news://www.plone.org">news</a></li>' +
-                '      <li><a href="irc://www.plone.org">irc</a></li>' +
-                '      <li><a href="h323://www.plone.org">h323</a></li>' +
-                '      <li><a href="sip://www.plone.org">sip</a></li>' +
-                '      <li><a href="callto://www.plone.org">callto</a></li>' +
-                '      <li><a href="feed://www.plone.org">feed</a></li>' +
-                '      <li><a href="webcal://www.plone.org">webcal</a></li>' +
-                "    </ul>" +
-                "</div>" +
-                '<div class="anchors pat-markspeciallinks">' +
-                '  <p>Find out What&#39s new in <a href="#anchor">Plone</a>.<br>' +
-                '     Plone is written in <a class="link-plain" href="http://www.python.org">Python</a>.' +
-                "  </p>" +
-                "</div>"
-        );
-        // const url = "https://www.plone.org/";
-        // location = window.location;
-        // const mockLocation = new URL(url);
-        // mockLocation.replace = jest.fn();
-        // delete window.location;
-        // window.location = mockLocation;
+    afterEach(function () {
+        document.body.innerHTML = "";
     });
-    it("normal external links have target=_blank", async function () {
-        const util = require("util");
+
+    it("external links have target=_blank", async function () {
+        document.body.innerHTML = `
+            <p id="id1" class="pat-markspeciallinks">
+              <a id="id11" href="./internal-link">internal link</a>
+              <a id="id12" href="#anchor">anchor</a>
+              <a id="id13" href="ftp://ftp.url">ftp url</a>
+              <a id="id14" class="link-plain" href="http://external.url">external url</a>
+              <a id="id15" class="link-plain" href="https://secure.url">secure url</a>
+              <a id="id16" href="http://external.url">external url</a>
+              <a id="id17" href="https://secure.url">secure url</a>
+            </p>
+            <p id="id2" class="pat-markspeciallinks" data-pat-markspeciallinks="external_links_open_new_window: true">
+              <a id="id21" href="./internal-link">internal link</a>
+              <a id="id22" href="#anchor">anchor</a>
+              <a id="id23" href="ftp://ftp.url">ftp url</a>
+              <a id="id24" class="link-plain" href="http://external.url">external url</a>
+              <a id="id25" class="link-plain" href="https://secure.url">secure url</a>
+              <a id="id26" href="http://external.url">external url</a>
+              <a id="id27" href="https://secure.url">secure url</a>
+            </p>
+        `;
+
+        registry.scan(document.body);
         await utils.timeout(1);
-        registry.scan(this.$el);
+
+        expect(document.querySelector("#id11").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id12").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id13").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id14").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id15").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id16").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id17").getAttribute("target")).toBe(null);
+
+        expect(document.querySelector("#id21").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id22").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id23").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id24").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id25").getAttribute("target")).toBe(null);
+        expect(document.querySelector("#id26").getAttribute("target")).toBe("_blank");
+        expect(document.querySelector("#id26").getAttribute("rel")).toBe("noopener");
+        expect(document.querySelector("#id27").getAttribute("target")).toBe("_blank");
+        expect(document.querySelector("#id27").getAttribute("rel")).toBe("noopener");
+    });
+
+    it("check for correct icon classes per protocol", async function () {
+        document.body.innerHTML = `
+            <ul class="pat-markspeciallinks">
+              <li id="id1" ><a href="http://www.plone.org">http</a></li>
+              <li id="id2" ><a href="https://www.plone.org">https</a></li>
+              <li id="id3" ><a href="mailto:info@plone.org">mailto</a></li>
+              <li id="id4" ><a href="ftp://www.plone.org">ftp</a></li>
+              <li id="id5" ><a href="news://www.plone.org">news</a></li>
+              <li id="id6" ><a href="irc://www.plone.org">irc</a></li>
+              <li id="id7" ><a href="sip://www.plone.org">sip</a></li>
+              <li id="id8" ><a href="callto://www.plone.org">callto</a></li>
+              <li id="id9"><a href="webcal://www.plone.org">webcal</a></li>
+            </ul>
+        `;
+
+        registry.scan(document.body);
         await utils.timeout(1);
-        var link = this.$el.find("a");
-        // expect(window.location.url).toEqual(url);
-        expect(link.eq(0).attr("target") === undefined).toEqual(true);
-        expect(link.eq(1).attr("target") === undefined).toEqual(true);
-        expect(link.eq(2).attr("target")).toEqual("_blank");
-        expect(link.eq(3).attr("target") === undefined).toEqual(true);
-        expect(link.eq(0).prev()[0].tagName).toEqual("I");
-        expect(link.eq(1).prev()[0].tagName).not.toEqual("I");
-        expect(link.eq(4).prev().length).toEqual(0);
-        expect(link.eq(5).prev()[0].tagName).not.toEqual("I");
-    });
-    it("check for correct icon classes per protocol", function () {
-        registry.scan(this.$el);
-        var listel = this.$el.next(".icons").find("li");
-        expect(listel.eq(0).find("i").hasClass("link-external")).toEqual(true);
-        expect(listel.eq(1).find("i").hasClass("link-https")).toEqual(true);
-        expect(listel.eq(2).find("i").hasClass("link-mailto")).toEqual(true);
-        expect(listel.eq(3).find("i").hasClass("link-ftp")).toEqual(true);
-        expect(listel.eq(4).find("i").hasClass("link-news")).toEqual(true);
-        expect(listel.eq(5).find("i").hasClass("link-irc")).toEqual(true);
-        expect(listel.eq(6).find("i").hasClass("link-h323")).toEqual(true);
-        expect(listel.eq(7).find("i").hasClass("link-sip")).toEqual(true);
-        expect(listel.eq(8).find("i").hasClass("link-callto")).toEqual(true);
-        expect(listel.eq(9).find("i").hasClass("link-feed")).toEqual(true);
-        expect(listel.eq(10).find("i").hasClass("link-webcal")).toEqual(true);
-    });
-    it("do not show the lock icon for anchor links", function () {
-        registry.scan(this.$el);
-        var link = this.$el.next(".anchors").find("p").find("a");
-        expect(link.eq(0).prev().length).toEqual(0);
+
+        expect(document.querySelector("#id1 svg").classList.contains("bi-link-45deg")).toBe(true); // prettier-ignore
+        expect(document.querySelector("#id2 svg").classList.contains("bi-link-45deg")).toBe(true); // prettier-ignore
+        expect(document.querySelector("#id3 svg").classList.contains("bi-envelope")).toBe(true); // prettier-ignore
+        expect(document.querySelector("#id4 svg").classList.contains("bi-cloud-download")).toBe(true); // prettier-ignore
+        expect(document.querySelector("#id5 svg").classList.contains("bi-newspaper")).toBe(true); // prettier-ignore
+        expect(document.querySelector("#id6 svg").classList.contains("bi-chat")).toBe(true); // prettier-ignore
+        expect(document.querySelector("#id7 svg").classList.contains("bi-telephone")).toBe(true); // prettier-ignore
+        expect(document.querySelector("#id8 svg").classList.contains("bi-telephone")).toBe(true); // prettier-ignore
+        expect(document.querySelector("#id9 svg").classList.contains("bi-calendar")).toBe(true); // prettier-ignore
     });
 });
