e451a626198d96de25e1aa75cc24237d302f9f4a
Johannes Raggam <thetetet@gmail.com>
2022-01-20 21:48:38 +0100

    maint(docs): 11ty cleanup.

--- .eleventy.js
+++ .eleventy.js
@@ -1,41 +1,20 @@
-// const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-// module.exports = function(eleventyConfig) {
-//   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-// };
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
     eleventyConfig.addPlugin(syntaxHighlight);
-    //eleventyConfig.addPassthroughCopy({"src/pat": "pat"});
     eleventyConfig.addPassthroughCopy({ "favicon.ico": "favicon.ico" });
     eleventyConfig.addPassthroughCopy({ "docs/main.css": "dist/main.css" });
     eleventyConfig.addPassthroughCopy({ dist: "dist" });
-    //   eleventyConfig.addPassthroughCopy({"src/tests/json":"dist/json"});
-    eleventyConfig.addPassthroughCopy({
-        "src/tests/fakeserver.js": "dist/fakeserver.js",
-    });
-    eleventyConfig.addPassthroughCopy({
-        "src/pat/querystring/test-querystringcriteria.json":
-            "dist/test-querystringcriteria.json",
-    });
-    eleventyConfig.addPassthroughCopy({
-        "node_modules/prism-themes": "dist/prism-themes",
-    });
+    eleventyConfig.addPassthroughCopy({ "src/tests/fakeserver.js": "dist/fakeserver.js" }); // prettier-ignore
+    eleventyConfig.addPassthroughCopy({ "src/pat/querystring/test-querystringcriteria.json": "dist/test-querystringcriteria.json" }); // prettier-ignore
+    eleventyConfig.addPassthroughCopy({ "node_modules/prism-themes": "dist/prism-themes" }); // prettier-ignore
     eleventyConfig.addPassthroughCopy({ "node_modules/sinon": "dist/sinon" });
     eleventyConfig.addPassthroughCopy({ "node_modules/tinymce/skins": "dist/skins" });
     eleventyConfig.addPassthroughCopy({ "node_modules/tinymce/icons": "dist/icons" });
     eleventyConfig.addPassthroughCopy({ "node_modules/tinymce/themes": "dist/themes" });
     eleventyConfig.addPassthroughCopy({ "node_modules/underscore": "dist/underscore" });
     eleventyConfig.addPassthroughCopy({ "node_modules/bootstrap": "dist/bootstrap" });
-    // eleventyConfig.addPassthroughCopy({"mockup/node_modules/requirejs/require.js": "mockup/build/require.js"});
-    // eleventyConfig.addPassthroughCopy({"mockup/node_modules/sinon/pkg/sinon.js": "mockup/build/sinon.js"});
-    // eleventyConfig.addPassthroughCopy({"mockup/node_modules/sinon/pkg/sinon-esm.js": "mockup/build/sinon-esm.js"});
-    //eleventyConfig.addPassthroughCopy("mockup/patterns/**/*.json");
-    //eleventyConfig.addPassthroughCopy({"../mockup/js/bundles": "js/bundles"});
-    //eleventyConfig.addPassthroughCopy({"../mockup/js/config.js": "js/config.js"});
-    //eleventyConfig.addPassthroughCopy({"../mockup/js/patterns": "patterns"});
-    //eleventyConfig.addPassthroughCopy({"../mockup/node_modules": "node_modules"});
 
     // Filter source file names using a glob
     eleventyConfig.addCollection("patterns", function (collectionApi) {
