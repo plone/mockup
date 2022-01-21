d7efe48b6dc72feb7b62fc31741673fc32e8f0e8
Manuel Reinhardt <reinhardt@syslab.com>
2022-01-21 14:13:38 +0100

    Ported livesearch test. 2 failures remaining, related to focus.

--- src/pat/livesearch/livesearch.test.js
+++ src/pat/livesearch/livesearch.test.js
@@ -1,118 +1,107 @@
-define([
-    "expect",
-    "jquery",
-    "sinon",
-    "pat-registry",
-    "mockup-patterns-livesearch",
-], function (expect, $, sinon, registry, Livesearch) {
-    "use strict";
+import "./livesearch";
+import Livesearch from "./livesearch";
+import $ from "jquery";
+import registry from "@patternslib/patternslib/src/core/registry";
+import sinon from "sinon";
 
-    window.mocha.setup("bdd");
-    $.fx.off = true;
+describe("Livesearch", function () {
+    beforeEach(function () {
+        this.server = sinon.fakeServer.create();
+        this.server.autoRespond = true;
 
-    /* ==========================
-   TEST: Livesearch 
-  ========================== */
-
-    describe("Livesearch", function () {
-        beforeEach(function () {
-            this.server = sinon.fakeServer.create();
-            this.server.autoRespond = true;
-
-            this.server.respondWith("GET", /livesearch\.json/, function (xhr, id) {
-                var items = [];
-                for (var i = 0; i < 7; i = i + 1) {
-                    items.push({
-                        url: "http://localhost:8081/news/aggregator",
-                        description: "Site News",
-                        title: "News",
-                        state: "published",
-                    });
-                }
-                xhr.respond(
-                    200,
-                    { "Content-Type": "application/json" },
-                    JSON.stringify({
-                        total: items.length * 2,
-                        items: items,
-                    })
-                );
-            });
-            this.clock = sinon.useFakeTimers();
-
-            this.$el = $(
-                "" +
-                    '<form class="pat-livesearch">' +
-                    ' <input type="text" value="foobar" />' +
-                    "</form>"
-            ).appendTo($("body"));
-
-            this.ls = new Livesearch(this.$el, {
-                ajaxUrl: "livesearch.json",
-            });
+        this.server.respondWith("GET", /livesearch\.json/, function (xhr, id) {
+            var items = [];
+            for (var i = 0; i < 7; i = i + 1) {
+                items.push({
+                    url: "http://localhost:8081/news/aggregator",
+                    description: "Site News",
+                    title: "News",
+                    state: "published",
+                });
+            }
+            xhr.respond(
+                200,
+                { "Content-Type": "application/json" },
+                JSON.stringify({
+                    total: items.length * 2,
+                    items: items,
+                })
+            );
         });
+        this.clock = sinon.useFakeTimers();
 
-        afterEach(function () {
-            $("body").empty();
-            this.server.restore();
-            this.clock.restore();
-            this.$el.remove();
+        this.$el = $(
+            "" +
+                '<form class="pat-livesearch">' +
+                ' <input type="text" value="foobar" />' +
+                "</form>"
+        ).appendTo($("body"));
+
+        this.ls = new Livesearch(this.$el, {
+            ajaxUrl: "livesearch.json",
         });
+    });
 
-        it("results on focus", function () {
-            this.ls.$input.trigger("focusin");
-            this.clock.tick(1000);
-            expect(this.$el.find(".livesearch-results li").length).to.equal(9);
-        });
+    afterEach(function () {
+        $("body").empty();
+        this.server.restore();
+        this.clock.restore();
+        this.$el.remove();
+    });
 
-        it("responds to keyup", function () {
-            this.ls.$input.trigger("keyup");
-            this.clock.tick(1000);
-            expect(this.$el.find(".livesearch-results li").length).to.equal(9);
-        });
+    it("results on focus", function () {
+        this.ls.$input.trigger("focusin");
+        this.clock.tick(1000);
+        expect(this.$el.find(".livesearch-results li").length).toEqual(9);
+    });
 
-        it("does not show with minimum length not met", function () {
-            this.ls.$input.attr("value", "fo");
-            this.ls.$input.trigger("keyup");
-            this.clock.tick(1000);
-            expect(this.$el.find(".livesearch-results li").length).to.equal(1);
-        });
+    it("responds to keyup", function () {
+        this.ls.$input.trigger("keyup");
+        this.clock.tick(1000);
+        expect(this.$el.find(".livesearch-results li").length).toEqual(9);
+    });
 
-        it("focus out hides", function () {
-            this.ls.$input.trigger("keyup");
-            this.clock.tick(1000);
-            expect(this.$el.find(".livesearch-results li").length).to.equal(9);
+    it("does not show with minimum length not met", function () {
+        this.ls.$input.attr("value", "fo");
+        this.ls.$input.trigger("keyup");
+        this.clock.tick(1000);
+        expect(this.$el.find(".livesearch-results li").length).toEqual(1);
+    });
 
-            this.ls.$input.trigger("focusout");
-            this.clock.tick(1000);
-            expect(this.$el.find(".livesearch-results").is(":visible")).to.equal(false);
-        });
+    it("focus out hides", function () {
+        this.ls.$input.trigger("keyup");
+        this.clock.tick(1000);
+        expect(this.$el.find(".livesearch-results li").length).toEqual(9);
 
-        it("focus back in shows already searched", function () {
-            this.ls.$input.trigger("keyup");
-            this.clock.tick(1000);
-            expect(this.$el.find(".livesearch-results li").length).to.equal(9);
+        this.ls.$input.trigger("focusout");
+        this.clock.tick(1000);
+        expect(this.$el.find(".livesearch-results").is(":visible")).toEqual(false);
+    });
 
-            this.ls.$input.trigger("focusout");
-            this.clock.tick(1000);
-            expect(this.$el.find(".livesearch-results").is(":visible")).to.equal(false);
+    it("focus back in shows already searched", function () {
+        this.ls.$input.trigger("keyup");
+        this.clock.tick(1000);
+        expect(this.$el.find(".livesearch-results li").length).toEqual(9);
 
-            this.ls.$input.trigger("focusin");
-            this.clock.tick(1000);
-            expect(this.$el.find(".livesearch-results").is(":visible")).to.equal(true);
-        });
+        this.ls.$input.trigger("focusout");
+        this.clock.tick(1000);
+        expect(this.$el.find(".livesearch-results").is(":visible")).toEqual(false);
 
-        it("should show next and prev", function () {
-            this.ls.$input.trigger("keyup");
-            this.clock.tick(1000);
-            expect(this.$el.find(".livesearch-results li").length).to.equal(9);
-            expect(this.$el.find(".livesearch-results li a.next").length).to.equal(1);
+        this.ls.$input.trigger("focusin");
+        this.clock.tick(1000);
+        expect(this.$el.find(".livesearch-results").is(":visible")).toEqual(true);
+    });
 
-            this.ls.doSearch(2);
-            this.clock.tick(1000);
+    it("should show next and prev", function () {
+        this.ls.$input.trigger("keyup");
+        this.clock.tick(1000);
+        expect(this.$el.find(".livesearch-results li").length).toEqual(9);
+        expect(this.$el.find(".livesearch-results li a.next").length).toEqual(1);
 
-            expect(this.$el.find(".livesearch-results li").length).to.equal(9);
-            expect(this.$el.find(".livesearch-results li a.prev").length).to.equal(1);
-        });
+        this.ls.doSearch(2);
+        this.clock.tick(1000);
+
+        expect(this.$el.find(".livesearch-results li").length).toEqual(9);
+        expect(this.$el.find(".livesearch-results li a.prev").length).toEqual(1);
     });
 });
