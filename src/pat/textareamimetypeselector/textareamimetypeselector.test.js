ea80519f06b947cb6f761cb753e303babc0720e7
Manuel Reinhardt <reinhardt@syslab.com>
2022-01-24 15:34:18 +0100

    Ported textareamimetypeselector test. 2 failures remaining, related to visibility.

--- src/pat/textareamimetypeselector/textareamimetypeselector.test.js
+++ src/pat/textareamimetypeselector/textareamimetypeselector.test.js
@@ -1,145 +1,132 @@
-define([
-    "expect",
-    "jquery",
-    "pat-registry",
-    "mockup-patterns-textareamimetypeselector",
-    "mockup-patterns-tinymce",
-], function (expect, $, registry, textareamimetypeselector, tinymce) {
-    "use strict";
+import "./textareamimetypeselector";
+import registry from "@patternslib/patternslib/src/core/registry";
+import $ from "jquery";
 
-    window.mocha.setup("bdd");
-    $.fx.off = true;
+describe("Textarea MimeType Selector", function () {
+    afterEach(function () {
+        $("body").empty();
+    });
 
-    /* ================================
-   TEST: Textarea MimeType Selector
-  ================================= */
+    it("Switching changes widget", function () {
+        var dom_structure =
+            '<textarea name="text">hello world</textarea>' +
+            "<select" +
+            '    name="text.mimeType"' +
+            '    class="pat-textareamimetypeselector"' +
+            "    data-pat-textareamimetypeselector='{" +
+            '      "textareaName": "text",' +
+            '      "widgets": {' +
+            '        "text/html": {' +
+            '          "pattern": "tinymce",' +
+            '          "patternOptions": {' +
+            '            "tiny": {' +
+            '              "plugins": [],' +
+            '              "menubar": "edit format tools",' +
+            '              "toolbar": " "' +
+            "            }" +
+            "          }" +
+            "        }" +
+            "      }" +
+            "    }'" +
+            "  >" +
+            '  <option value="text/html">text/html</option>' +
+            '  <option value="text/plain" selected="selected">text/plain</option>' +
+            "</select>";
 
-    describe("Textarea MimeType Selector", function () {
-        afterEach(function () {
-            $("body").empty();
-        });
+        var $doc = $(dom_structure).appendTo("body");
+        registry.scan($doc);
 
-        it("Switching changes widget", function () {
-            var dom_structure =
-                '<textarea name="text">hello world</textarea>' +
-                "<select" +
-                '    name="text.mimeType"' +
-                '    class="pat-textareamimetypeselector"' +
-                "    data-pat-textareamimetypeselector='{" +
-                '      "textareaName": "text",' +
-                '      "widgets": {' +
-                '        "text/html": {' +
-                '          "pattern": "tinymce",' +
-                '          "patternOptions": {' +
-                '            "tiny": {' +
-                '              "plugins": [],' +
-                '              "menubar": "edit format tools",' +
-                '              "toolbar": " "' +
-                "            }" +
-                "          }" +
-                "        }" +
-                "      }" +
-                "    }'" +
-                "  >" +
-                '  <option value="text/html">text/html</option>' +
-                '  <option value="text/plain" selected="selected">text/plain</option>' +
-                "</select>";
+        var $el = $("[name='text.mimeType']");
+        var $textarea = $("[name='text']");
 
-            var $doc = $(dom_structure).appendTo("body");
-            registry.scan($doc);
+        // Initially, text/plain is selected and textarea should be visible.
+        expect($textarea.is(":visible")).toEqual(true);
+        // But TinyMCE shouldn't be there
+        expect($(".mce-tinymce").length).toEqual(0);
+        // Value should be at it's initial state
+        expect($textarea.val()).toEqual("hello world");
 
-            var $el = $("[name='text.mimeType']");
-            var $textarea = $("[name='text']");
+        // Now, select text/html
+        $el.val("text/html").change();
 
-            // Initially, text/plain is selected and textarea should be visible.
-            expect($textarea.is(":visible")).to.be.ok();
-            // But TinyMCE shouldn't be there
-            expect($(".mce-tinymce").length).to.be(0);
-            // Value should be at it's initial state
-            expect($textarea.val()).to.be.equal("hello world");
+        // Textarea should be hidden
+        expect($textarea.is(":hidden")).toEqual(true);
+        // And TinyMCE should be shown
+        expect($(".mce-tinymce").is(":visible")).toEqual(true);
 
-            // Now, select text/html
-            $el.val("text/html").change();
+        // Switching back to text/plain should destroy TinyMCE
+        $el.val("text/plain").change();
+        expect($textarea.is(":visible")).toEqual(true);
+        expect($(".mce-tinymce").length).toEqual(0);
+        // Unfortunately, TinyMCE changes the value just by loading TinyMCE.
+        expect($textarea.val()).toEqual("hello world");
+    });
 
-            // Textarea should be hidden
-            expect($textarea.is(":hidden")).to.be.ok();
-            // And TinyMCE should be shown
-            expect($(".mce-tinymce").is(":visible")).to.be.ok();
+    it("Switching changes widget with inline TinyMCE", function () {
+        var dom_structure =
+            '<textarea name="text">hello world</textarea>' +
+            "<select" +
+            '    name="text.mimeType"' +
+            '    class="pat-textareamimetypeselector"' +
+            "    data-pat-textareamimetypeselector='{" +
+            '      "textareaName": "text",' +
+            '      "widgets": {' +
+            '        "text/html": {' +
+            '          "pattern": "tinymce",' +
+            '          "patternOptions": {' +
+            '            "inline": true,' +
+            '            "tiny": {' +
+            '              "plugins": [],' +
+            '              "menubar": "edit format tools",' +
+            '              "toolbar": " "' +
+            "            }" +
+            "          }" +
+            "        }" +
+            "      }" +
+            "    }'" +
+            "  >" +
+            '  <option value="text/html">text/html</option>' +
+            '  <option value="text/plain" selected="selected">text/plain</option>' +
+            "</select>";
 
-            // Switching back to text/plain should destroy TinyMCE
-            $el.val("text/plain").change();
-            expect($textarea.is(":visible")).to.be.ok();
-            expect($(".mce-tinymce").length).to.be(0);
-            // Unfortunately, TinyMCE changes the value just by loading TinyMCE.
-            expect($textarea.val()).to.be.equal("hello world");
-        });
+        var $doc = $(dom_structure).appendTo("body");
+        registry.scan($doc);
 
-        it("Switching changes widget with inline TinyMCE", function () {
-            var dom_structure =
-                '<textarea name="text">hello world</textarea>' +
-                "<select" +
-                '    name="text.mimeType"' +
-                '    class="pat-textareamimetypeselector"' +
-                "    data-pat-textareamimetypeselector='{" +
-                '      "textareaName": "text",' +
-                '      "widgets": {' +
-                '        "text/html": {' +
-                '          "pattern": "tinymce",' +
-                '          "patternOptions": {' +
-                '            "inline": true,' +
-                '            "tiny": {' +
-                '              "plugins": [],' +
-                '              "menubar": "edit format tools",' +
-                '              "toolbar": " "' +
-                "            }" +
-                "          }" +
-                "        }" +
-                "      }" +
-                "    }'" +
-                "  >" +
-                '  <option value="text/html">text/html</option>' +
-                '  <option value="text/plain" selected="selected">text/plain</option>' +
-                "</select>";
+        var $el = $("[name='text.mimeType']");
+        var $textarea = $("[name='text']");
 
-            var $doc = $(dom_structure).appendTo("body");
-            registry.scan($doc);
+        // Initially, text/plain is selected and textarea should be visible.
+        expect($textarea.is(":visible")).toEqual(true);
+        // But TinyMCE shouldn't be there
+        expect($(".mce-content-body[contenteditable='true']").length).toEqual(0);
+        // Value should be at it's initial state
+        expect($textarea.val()).toEqual("hello world");
 
-            var $el = $("[name='text.mimeType']");
-            var $textarea = $("[name='text']");
+        // change the textarea's value
+        $textarea.val("hello mellow");
 
-            // Initially, text/plain is selected and textarea should be visible.
-            expect($textarea.is(":visible")).to.be.ok();
-            // But TinyMCE shouldn't be there
-            expect($(".mce-content-body[contenteditable='true']").length).to.be(0);
-            // Value should be at it's initial state
-            expect($textarea.val()).to.be.equal("hello world");
+        // Now, select text/html
+        $el.val("text/html").change();
 
-            // change the textarea's value
-            $textarea.val("hello mellow");
+        // Textarea should be hidden
+        expect($textarea.is(":hidden")).toEqual(true);
+        // And TinyMCE should be shown
+        expect(
+            $(".mce-content-body[contenteditable='true']").is(":visible")
+        ).toEqual(true);
 
-            // Now, select text/html
-            $el.val("text/html").change();
+        // Switching back to text/plain should destroy TinyMCE
+        $el.val("text/plain").change();
+        expect($textarea.is(":visible")).toEqual(true);
+        expect($(".mce-content-body[contenteditable='true']").length).toEqual(0);
+        // Unfortunately, TinyMCE changes the value just by loading TinyMCE.
+        expect($textarea.val()).toEqual("<p>hello mellow</p>");
 
-            // Textarea should be hidden
-            expect($textarea.is(":hidden")).to.be.ok();
-            // And TinyMCE should be shown
-            expect(
-                $(".mce-content-body[contenteditable='true']").is(":visible")
-            ).to.be.ok();
-
-            // Switching back to text/plain should destroy TinyMCE
-            $el.val("text/plain").change();
-            expect($textarea.is(":visible")).to.be.ok();
-            expect($(".mce-content-body[contenteditable='true']").length).to.be(0);
-            // Unfortunately, TinyMCE changes the value just by loading TinyMCE.
-            expect($textarea.val()).to.be.equal("<p>hello mellow</p>");
-
-            // switching back to TinyMCE loads it again
-            $el.val("text/html").change();
-            expect($textarea.is(":hidden")).to.be.ok();
-            expect(
-                $(".mce-content-body[contenteditable='true']").is(":visible")
-            ).to.be.ok();
-        });
+        // switching back to TinyMCE loads it again
+        $el.val("text/html").change();
+        expect($textarea.is(":hidden")).toEqual(true);
+        expect(
+            $(".mce-content-body[contenteditable='true']").is(":visible")
+        ).toEqual(true);
     });
 });
