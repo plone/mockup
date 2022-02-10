324f1cc28d52a6abb7f51c69a2c049cf293821be
petschki <peter.mathis@kombinat.at>
2022-02-10 10:03:45 +0100

    fix toolbar test

--- src/pat/toolbar/toolbar.test.js
+++ src/pat/toolbar/toolbar.test.js
@@ -1,14 +1,27 @@
-import Pattern from "./toolbar";
+import "./toolbar";
+import registry from "@patternslib/patternslib/src/core/registry";
+import utils from "@patternslib/patternslib/src/core/utils";
 
 describe("Toolbar", function () {
-    it("Initializes correctly", function () {
+    beforeEach(function () {
         document.body.innerHTML = `
             <div id="edit-zone">
-                <div class="pat-toolbar" />
+                <div class="offcanvas-toggler">
+                    <a class="toggler-hover" data-bs-toggle="offcanvas" href="#edit-zone"></a>
+                </div>
+                <div class="pat-toolbar offcanvas" />
             </div>
         `;
-        const el = document.body.querySelector(".pat-toolbar");
-        new Pattern(el);
-        expect(el.classList.contains("initialized")).toBe(true);
+    });
+
+    afterEach(function () {
+        document.body.outerHTML = "";
+    });
+
+    it("Initializes correctly", async function () {
+        expect(document.body.querySelectorAll(".pat-toolbar.initialized").length).toBe(0);
+        registry.scan(document.body);
+        await utils.timeout(1);
+        expect(document.body.querySelectorAll(".pat-toolbar.initialized").length).toBe(1);
     });
 });
