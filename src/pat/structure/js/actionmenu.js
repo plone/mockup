aea3272ebbd6c0c832d29b7c8c126866d9e5747f
Franco Pellegrini <frapell@gmail.com>
2022-03-28 17:56:39 -0300

    use .cloneDeep from lodash instead of .clone from underscore

--- src/pat/structure/js/actionmenu.js
+++ src/pat/structure/js/actionmenu.js
@@ -1,4 +1,4 @@
-import _ from "underscore";
+import _ from "lodash";
 import _t from "../../../core/i18n-wrapper";
 
 const menuOptions = {
@@ -93,7 +93,7 @@ const ActionMenu = function (menu) {
     const model = menu.model.attributes;
     const app = menu.app;
 
-    const result = _.clone(menuOptions);
+    const result = _.cloneDeep(menuOptions);
     if (!(app.pasteAllowed() && model.is_folderish)) {
         delete result.pasteItem;
     }
