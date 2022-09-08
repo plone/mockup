c4347f41c1dfab628a4dc1a7e7658756d0d47fbe
Johannes Raggam <thetetet@gmail.com>
2022-09-08 18:09:46 +0200

    maint(pat navigationmarker): Simplify and improve the implementation.

--- src/pat/navigationmarker/navigationmarker.js
+++ src/pat/navigationmarker/navigationmarker.js
@@ -1,54 +1,87 @@
-import $ from "jquery";
 import Base from "@patternslib/patternslib/src/core/base";
+import Parser from "@patternslib/patternslib/src/core/parser";
+
+export const parser = new Parser("navigation");
+parser.addArgument("item-wrapper", null);
+parser.addArgument("in-path-class", "inPath");
+parser.addArgument("current-class", "current");
 
 export default Base.extend({
     name: "navigationmarker",
     trigger: ".pat-navigationmarker",
     parser: "mockup",
     init() {
-        const portal_url = document.body.dataset.portalUrl;
-        const href =
-            document.querySelector('head link[rel="canonical"]').href ||
-            window.location.href;
-        const hrefParts = href.split("/");
+        this.options = parser.parse(this.el, this.options);
+        this.mark_items();
+    },
 
+    mark_items(url) {
+        // Mark all navigation items that are in the path of the current url
+
+        const current_url = url || this.base_url();
+        const current_url_prepared = this.prepare_url(current_url);
+
+        const portal_url = this.prepare_url(document.body.dataset?.portalUrl);
         const nav_items = this.el.querySelectorAll("a");
 
         for (const nav_item of nav_items) {
-            const navlink = nav_item.getAttribute("href", "").replace("/view", "");
-            if (href.indexOf(navlink) !== -1) {
-                const parent = $(nav_item).parent();
+            // Get the nav item's url and rebase it against the current url to
+            // make absolute or relative URLs FQDN URLs.
+            const nav_url = this.prepare_url(
+                new URL(nav_item.getAttribute("href", ""), current_url)?.href
+            );
 
-                // check the input-openers within the path
-                const check = parent.find("> input");
-                if (check.length) {
-                    check[0].checked = true;
-                }
+            const wrapper = this.options.itemWrapper
+                ? nav_item.closest(this.options.itemWrapper)
+                : nav_item.parentNode;
 
-                // set "inPath" to all nav items which are within the current path
-                // check if parts of navlink are in canonical url parts
-                const navParts = navlink.split("/");
-                let inPath = false;
-                for (let i = 0, size = navParts.length; i < size; i++) {
-                    // The last path-part must match.
-                    inPath = false;
-                    if (navParts[i] === hrefParts[i]) {
-                        inPath = true;
-                    }
-                }
-                if (navlink === portal_url && href !== portal_url) {
-                    // Avoid marking "Home" with "inPath", when not actually there
-                    inPath = false;
-                }
-                if (inPath) {
-                    parent.addClass("inPath");
-                }
-
-                // set "current" to the current selected nav item, if it is in the navigation structure.
-                if (href === navlink) {
-                    parent.addClass("current");
-                }
+            if (nav_url === current_url_prepared) {
+                wrapper.classList.add(this.options.currentClass);
+            } else if (
+                // Compare the current navigation item url with a slash at the
+                // end - if it is "inPath" it must have a slash in it.
+                current_url_prepared.indexOf(`${nav_url}/`) === 0 &&
+                // Do not set inPath for the "Home" url, as this would always
+                // be in the path.
+                nav_url !== portal_url
+            ) {
+                wrapper.classList.add(this.options.inPathClass);
+            } else {
+                // Not even in path.
+                continue;
             }
+
+            // The path was at least found in the current url, so we need to
+            // check the input-openers within the path
+            // Find the first input which is the correct one, even if this
+            // navigation item has many children.
+            // These hidden checkboxes are used to open the navigation item for
+            // mobile navigation.
+            const check = wrapper.querySelector("input");
+            if (check) check.checked = true;
         }
+    },
+
+    clear_items() {
+        // Clear all navigation items from the inPath and current classes
+
+        const items = this.el.querySelectorAll(
+            `.${this.options.inPathClass}, .${this.options.currentClass}`
+        );
+        for (const item of items) {
+            item.classList.remove(this.options.inPathClass);
+            item.classList.remove(this.options.currentClass);
+        }
+    },
+
+    prepare_url(url) {
+        return url?.replace("/view", "").replaceAll("@@", "").replace(/\/$/, "");
+    },
+
+    base_url() {
+        return this.prepare_url(
+            document.querySelector('head link[rel="canonical"]')?.href ||
+                window.location.href
+        );
     },
 });
