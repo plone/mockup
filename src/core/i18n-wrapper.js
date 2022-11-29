9a8858de3f0120e740281075c4c7902111e16164
Peter Mathis <peter.mathis@kombinat.at>
2022-11-29 10:45:09 +0100

    feat(i18n): export "plone" and "widgets" translation domains

--- src/core/i18n-wrapper.js
+++ src/core/i18n-wrapper.js
@@ -12,14 +12,33 @@ import I18N from "./i18n";
 // we're creating a singleton here so we can potentially
 // delay the initialization of the translate catalog
 // until after the dom is available
-let _t = null;
-const translate = function (msgid, keywords) {
-    if (_t === null) {
-        var i18n = new I18N();
-        i18n.loadCatalog("widgets");
-        _t = i18n.MessageFactory("widgets");
-    }
-    return _t(msgid, keywords);
+let _translation_domains = {
+    "plone": null,
+    "widgets": null,
 };
 
-export default translate;
+const translate = function(domain, msgid, keywords) {
+    if(_translation_domains[domain] === null) {
+        var i18n = new I18N();
+        i18n.loadCatalog(domain);
+        _translation_domains[domain] = i18n.MessageFactory(domain);
+    }
+    return _translation_domains[domain](msgid, keywords);
+};
+
+const translate_widgets = function(msgid, keywords) {
+    return translate("widgets", msgid, keywords);
+};
+
+const translate_plone = function(msgid, keywords) {
+    return translate("plone", msgid, keywords);
+};
+
+// backwards compatibilty: "widgets" domain is default factory
+export default translate_widgets;
+
+// explicit export of translation domain factories
+export {
+    translate_widgets,
+    translate_plone,
+}
