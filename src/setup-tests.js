238ff3b19b8da756285a306defadc00ae163b908
Johannes Raggam <thetetet@gmail.com>
2022-12-11 15:35:06 +0100

    maint: Maintain own test setup config.
    Don't extend from @patternslib/patternslib for setting up tests.
    The config files are not included in the npm package distributions anymore.

---
+++ src/setup-tests.js
@@ -0,0 +1,21 @@
+// Extra test setup.
+
+import jquery from "jquery";
+global.$ = global.jQuery = jquery;
+
+jquery.expr.pseudos.visible = function () {
+    // Fix jQuery ":visible" selector always returns false in JSDOM.
+    // https://github.com/jsdom/jsdom/issues/1048#issuecomment-401599392
+    return true;
+};
+
+// Do not output error messages
+import logging from "@patternslib/patternslib/core/logging";
+logging.setLevel(50);
+// level: FATAL
+
+// patch dom.is_visible to not rely on jest-unavailable offsetWidth/Height
+import dom from "@patternslib/patternslib/core/dom";
+dom.is_visible = (el) => {
+    return !el.hidden && el.style.display !== "none";
+};
