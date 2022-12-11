15576797e51338d4345a90e242ede190d861f6b1
Johannes Raggam <thetetet@gmail.com>
2022-12-12 00:41:54 +0100

    maint(pat-texteditor): Use class based extension of @patternslib/pat-code-editor.

--- src/pat/texteditor/texteditor.js
+++ src/pat/texteditor/texteditor.js
@@ -1,9 +1,11 @@
 import CodeEditor from "@patternslib/pat-code-editor/src/code-editor.js";
 import Registry from "@patternslib/patternslib/src/core/registry";
 
-const Pattern = CodeEditor.extend({
-    name: "texteditor",
-    trigger: ".pat-texteditor",
-});
+class Pattern extends CodeEditor {
+    static name = "texteditor";
+    static trigger = ".pat-texteditor";
+}
+
 Registry.register(Pattern);
 export default Pattern;
+export { Pattern };
