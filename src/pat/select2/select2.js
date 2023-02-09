5588e96ef1100e3a3522a719f7765eb53646de4a
Johannes Raggam <thetetet@gmail.com>
2023-02-09 12:44:57 +0100

    maint(pat-select2): Modernize code.

--- src/pat/select2/select2.js
+++ src/pat/select2/select2.js
@@ -10,77 +10,80 @@ export default Base.extend({
     defaults: {
         separator: ",",
     },
-    initializeValues: function () {
-        var self = this;
+
+    initializeValues() {
         // Init Selection ---------------------------------------------
-        if (self.options.initialValues) {
-            self.options.id = function (term) {
+        if (this.options.initialValues) {
+            this.options.id = (term) => {
                 return term.id;
             };
-            self.options.initSelection = function ($el, callback) {
-                var data = [],
-                    value = $el.val(),
-                    seldefaults = self.options.initialValues;
+            this.options.initSelection = ($el, callback) => {
+                const data = [];
+                const value = $el.val();
+                let seldefaults = this.options.initialValues;
 
                 // Create the initSelection value that contains the default selection,
                 // but in a javascript object
                 if (
-                    typeof self.options.initialValues === "string" &&
-                    self.options.initialValues !== ""
+                    typeof this.options.initialValues === "string" &&
+                    this.options.initialValues !== ""
                 ) {
                     // if default selection value starts with a '{', then treat the value as
                     // a JSON object that needs to be parsed
-                    if (self.options.initialValues[0] === "{") {
-                        seldefaults = JSON.parse(self.options.initialValues);
+                    if (this.options.initialValues[0] === "{") {
+                        seldefaults = JSON.parse(this.options.initialValues);
                     }
                     // otherwise, treat the value as a list, separated by the defaults.separator value of
                     // strings in the format "id:text", and convert it to an object
                     else {
                         seldefaults = {};
-                        $(self.options.initialValues.split(self.options.separator)).each(
-                            function () {
-                                var selection = this.split(":");
-                                var id = $.trim(selection[0]);
-                                var text = $.trim(selection[1]);
-                                seldefaults[id] = text;
-                            }
+                        const initial_values = $(
+                            this.options.initialValues.split(this.options.separator)
                         );
+                        for (const it of initial_values) {
+                            const selection = it.split(":");
+                            const id = selection[0].trim();
+                            const text = selection[1].trim();
+                            seldefaults[id] = text;
+                        }
                     }
                 }
 
-                $(value.split(self.options.separator)).each(function () {
-                    var text = this;
-                    if (seldefaults[this]) {
-                        text = seldefaults[this];
+                const items = $(value.split(this.options.separator));
+                for (const it of items) {
+                    let text = it;
+                    if (seldefaults[it]) {
+                        text = seldefaults[it];
                     }
                     data.push({
-                        id: utils.removeHTML(this),
+                        id: utils.removeHTML(it),
                         text: utils.removeHTML(text),
                     });
-                });
+                }
                 callback(data);
             };
         }
     },
-    initializeTags: function () {
-        var self = this;
-        if (self.options.tags && typeof self.options.tags === "string") {
-            if (self.options.tags.substr(0, 1) === "[") {
-                self.options.tags = JSON.parse(self.options.tags);
+
+    initializeTags() {
+        if (this.options.tags && typeof this.options.tags === "string") {
+            if (this.options.tags.substr(0, 1) === "[") {
+                this.options.tags = JSON.parse(this.options.tags);
             } else {
-                self.options.tags = self.options.tags.split(self.options.separator);
+                this.options.tags = this.options.tags.split(this.options.separator);
             }
         }
 
-        if (self.options.tags && !self.options.allowNewItems) {
-            self.options.data = $.map(self.options.tags, function (value, i) {
+        if (this.options.tags && !this.options.allowNewItems) {
+            this.options.data = this.options.tags.map((value) => {
                 return { id: value, text: value };
             });
-            self.options.multiple = true;
-            delete self.options.tags;
+            this.options.multiple = true;
+            delete this.options.tags;
         }
     },
-    initializeOrdering: async function () {
+
+    async initializeOrdering() {
         if (!this.options.orderable) {
             return;
         }
@@ -98,7 +101,8 @@ export default Base.extend({
         this.$select2_el.on("change", _initializeOrdering.bind(this));
         _initializeOrdering();
     },
-    initializeSelect2: async function () {
+
+    async initializeSelect2() {
         import("select2/select2.css");
         import("./select2.scss");
         await import("select2");
@@ -111,8 +115,7 @@ export default Base.extend({
             console.warn("Language file could not be loaded", this.options.language);
         }
 
-        var self = this;
-        self.options.formatResultCssClass = function (ob) {
+        this.options.formatResultCssClass = (ob) => {
             if (ob.id) {
                 return (
                     "select2-option-" +
@@ -123,7 +126,7 @@ export default Base.extend({
 
         function callback(action, e) {
             if (action) {
-                if (self.options.debug) {
+                if (this.options.debug) {
                     console.debug("callback", action, e);
                 }
                 if (typeof action === "string") {
@@ -135,65 +138,68 @@ export default Base.extend({
             }
         }
 
-        this.$select2_el.select2(self.options);
-        this.$select2_el.on("select2-selected", function (e) {
-            callback(self.options.onSelected, e);
-        });
-        this.$select2_el.on("select2-selecting", function (e) {
-            callback(self.options.onSelecting, e);
-        });
-        this.$select2_el.on("select2-deselecting", function (e) {
-            callback(self.options.onDeselecting, e);
-        });
-        this.$select2_el.on("select2-deselected", function (e) {
-            callback(self.options.onDeselected, e);
-        });
+        this.$select2_el = $(this.$select2_el); // Make select2 available on
+        this.$select2_el.select2(this.options);
+        this.$select2_el.on("select2-selected", (e) =>
+            callback(this.options.onSelected, e)
+        );
+        this.$select2_el.on("select2-selecting", (e) =>
+            callback(this.options.onSelecting, e)
+        );
+        this.$select2_el.on("select2-deselecting", (e) =>
+            callback(this.options.onDeselecting, e)
+        );
+        this.$select2_el.on("select2-deselected", (e) =>
+            callback(this.options.onDeselected, e)
+        );
         this.$select2 = this.$select2_el.parent().find(".select2-container");
         this.$select2_el.parent().off("close.plone-modal.patterns");
-        if (self.options.orderable) {
-            self.$select2.addClass("select2-orderable");
+        if (this.options.orderable) {
+            this.$select2.addClass("select2-orderable");
         }
     },
-    opened: function () {
-        var isOpen = $(".select2-dropdown-open", this.$select2_el.parent()).length === 1;
+
+    opened() {
+        const isOpen =
+            $(".select2-dropdown-open", this.$select2_el.parent()).length === 1;
         return isOpen;
     },
-    init: async function () {
-        var self = this;
+
+    async init() {
         this.$select2_el = this.$el;
-        var i18n = new I18n();
-        self.options.language = i18n.currentLanguage;
-        self.options.allowNewItems = self.options.hasOwnProperty("allowNewItems")
-            ? JSON.parse(self.options.allowNewItems)
+        const i18n = new I18n();
+        this.options.language = i18n.currentLanguage;
+        this.options.allowNewItems = this.options.allowNewItems
+            ? JSON.parse(this.options.allowNewItems)
             : true;
 
         if (this.el.hasAttribute("multiple")) {
-            self.options.multiple = true;
+            this.options.multiple = true;
         }
 
-        if (self.options.ajax || self.options.vocabularyUrl) {
-            if (self.options.vocabularyUrl) {
-                self.options.multiple =
-                    self.options.multiple === undefined ? true : self.options.multiple;
-                self.options.ajax = self.options.ajax || {};
-                self.options.ajax.url = self.options.vocabularyUrl;
+        if (this.options.ajax || this.options.vocabularyUrl) {
+            if (this.options.vocabularyUrl) {
+                this.options.multiple =
+                    this.options.multiple === undefined ? true : this.options.multiple;
+                this.options.ajax = this.options.ajax || {};
+                this.options.ajax.url = this.options.vocabularyUrl;
                 // XXX removing the following function does'nt break tests. dead code?
-                self.options.initSelection = function ($el, callback) {
-                    var data = [],
-                        value = $el.val();
-                    $(value.split(self.options.separator)).each(function () {
-                        var val = utils.removeHTML(this);
-                        data.push({ id: val, text: val });
-                    });
+                this.options.initSelection = ($el, callback) => {
+                    const data = [];
+                    const value = $el.val();
+                    for (const val of value.split(this.options.separator)) {
+                        const _val = utils.removeHTML(val);
+                        data.push({ id: _val, text: _val });
+                    }
                     callback(data);
                 };
             }
 
-            var queryTerm = "";
-            self.options.ajax = $.extend(
+            let queryTerm = "";
+            this.options.ajax = $.extend(
                 {
                     quietMillis: 300,
-                    data: function (term, page) {
+                    data: (term, page) => {
                         queryTerm = term;
                         return {
                             query: term,
@@ -201,18 +207,18 @@ export default Base.extend({
                             page: page,
                         };
                     },
-                    results: function (data, page) {
-                        var results = data.results;
-                        if (self.options.vocabularyUrl) {
-                            var dataIds = [];
-                            $.each(data.results, function (i, item) {
-                                dataIds.push(item.id);
-                            });
+                    results: (data) => {
+                        let results = data.results;
+                        if (this.options.vocabularyUrl) {
+                            const dataIds = [];
+                            for (const it of data.results) {
+                                dataIds.push(it.id);
+                            }
                             results = [];
 
-                            var haveResult =
-                                queryTerm === "" || $.inArray(queryTerm, dataIds) >= 0;
-                            if (self.options.allowNewItems && !haveResult) {
+                            const haveResult =
+                                queryTerm === "" || dataIds.includes(queryTerm);
+                            if (this.options.allowNewItems && !haveResult) {
                                 queryTerm = utils.removeHTML(queryTerm);
                                 results.push({
                                     id: queryTerm,
@@ -220,34 +226,37 @@ export default Base.extend({
                                 });
                             }
 
-                            $.each(data.results, function (i, item) {
-                                results.push(item);
-                            });
+                            for (const it of data.results) {
+                                results.push(it);
+                            }
                         }
                         return { results: results };
                     },
                 },
-                self.options.ajax
+                this.options.ajax
             );
-        } else if (self.options.multiple && self.$el.is("select")) {
+        } else if (this.options.multiple && this.$el.is("select")) {
             // Multiselects need to be converted to input[type=hidden]
             // for Select2
-            var vals = self.$el.val() || [];
-            var options = $.map(self.$el.find("option"), function (o) {
-                return { text: $(o).html(), id: o.value };
+            const vals = this.$el.val() || [];
+            const options = [...this.el.querySelectorAll("option")].map((it) => {
+                return { text: it.innerHTML, id: it.value };
             });
-            this.$select2_el = $('<input type="hidden" />');
-            this.$select2_el.val(vals.join(self.options.separator));
-            this.$select2_el.attr("class", self.$el.attr("class"));
-            this.$select2_el.attr("name", self.$el.attr("name"));
-            this.$select2_el.attr("id", self.$el.attr("id"));
-            this.el.after(this.$select2_el[0]);
+
+            const el = document.createElement("input");
+            el.type = "hidden";
+            el.value = vals.join(this.options.separator);
+            el.className = this.el.getAttribute("class");
+            el.name = this.el.name;
+            el.id = this.el.id;
+            this.el.after(el);
             this.el.remove();
-            self.options.data = options;
+            this.$select2_el = $(el);
+            this.options.data = options;
         }
-        self.initializeValues();
-        self.initializeTags();
-        await self.initializeSelect2();
-        await self.initializeOrdering();
+        this.initializeValues();
+        this.initializeTags();
+        await this.initializeSelect2();
+        await this.initializeOrdering();
     },
 });
