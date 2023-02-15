537219ec04bb0562d215409de8b08ee55935193a
Johannes Raggam <thetetet@gmail.com>
2023-02-15 20:36:50 +0100

    maint: Remove regenerator-runtime. This is not needed anymore.

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const config = require("@patternslib/dev/jest.config.js");
 
 config.setupFilesAfterEnv.push(path.resolve(__dirname, "./src/setup-tests.js"));
 config.transformIgnorePatterns = [
-    "/node_modules/(?!@patternslib/)(?!@plone/)(?!preact/)(?!screenfull/)(?!bootstrap/)(?!datatable/)(?!regenerator-runtime/).+\\.[t|j]sx?$",
+    "/node_modules/(?!@patternslib/)(?!@plone/)(?!preact/)(?!screenfull/)(?!bootstrap/)(?!datatable/).+\\.[t|j]sx?$",
 ];
 
 module.exports = config;
