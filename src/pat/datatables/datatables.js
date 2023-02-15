67ad865bbd659b5c2b5626ce120bcc3c278aac66
Johannes Raggam <thetetet@gmail.com>
2023-02-15 21:18:59 +0100

    maint(pat-datatables): Actually, include all the datatables libraries.
    We're depending on them anyways, so let's import all.
    It's just a few kb of Bootstrap initialization code.

--- src/pat/datatables/datatables.js
+++ src/pat/datatables/datatables.js
@@ -15,28 +15,28 @@ export default Base.extend({
         import("datatables.net-bs5/css/dataTables.bootstrap5.min.css");
         import("datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css");
         import("datatables.net-colreorder-bs5/css/colReorder.bootstrap5.min.css");
+        import("datatables.net-fixedcolumns-bs5/css/fixedColumns.bootstrap5.min.css"); // prettier-ignore
+        import("datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.min.css"); // prettier-ignore
         import("datatables.net-rowreorder-bs5/css/rowReorder.bootstrap5.min.css");
         import("datatables.net-select-bs5/css/select.bootstrap5.min.css");
-        import("datatables.net-fixedheader-bs5/css/fixedHeader.bootstrap5.min.css"); // prettier-ignore
-        import("datatables.net-fixedcolumns-bs5/css/fixedColumns.bootstrap5.min.css"); // prettier-ignore
 
         await import("datatables.net");
         await import("datatables.net-bs5");
         await import("datatables.net-buttons");
-        //await import("datatables.net-buttons-bs5");
+        await import("datatables.net-buttons-bs5");
         await import("datatables.net-buttons/js/buttons.colVis");
         await import("datatables.net-buttons/js/buttons.html5");
         await import("datatables.net-buttons/js/buttons.print");
         await import("datatables.net-colreorder");
-        //await import("datatables.net-colreorder-bs5");
+        await import("datatables.net-colreorder-bs5");
         await import("datatables.net-fixedcolumns");
-        //await import("datatables.net-fixedcolumns-bs5");
+        await import("datatables.net-fixedcolumns-bs5");
         await import("datatables.net-fixedheader");
-        //await import("datatables.net-fixedheader-bs5");
+        await import("datatables.net-fixedheader-bs5");
         await import("datatables.net-rowreorder");
-        //await import("datatables.net-rowreorder-bs5");
+        await import("datatables.net-rowreorder-bs5");
         await import("datatables.net-select");
-        //await import("datatables.net-select-bs5");
+        await import("datatables.net-select-bs5");
 
         $(this.el).DataTable(this.options);
     },
