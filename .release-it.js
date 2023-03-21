14f84e0b3db6df732a3e4c6296879744136de0bf
Peter Mathis <peter.mathis@kombinat.at>
2023-03-21 15:41:19 +0100

    maint: override  for release-it

--- .release-it.js
+++ .release-it.js
@@ -1,1 +1,3 @@
-module.exports = require("@patternslib/dev/.release-it.js");
+const patternslib_dev_conf = require("@patternslib/dev/.release-it.js");
+patternslib_dev_conf.git.requireBranch = "5.0.x";
+module.exports = patternslib_dev_conf;
