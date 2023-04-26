818313da783b58a93d8d774260c752108cb99c23
Peter Mathis <peter.mathis@kombinat.at>
2023-04-26 08:27:07 +0200

    fix(pat reccurrence): Fix default selected range option if there's no "repeat forever" button (which is default in the event behavior).

--- src/pat/recurrence/recurrence.js
+++ src/pat/recurrence/recurrence.js
@@ -606,7 +606,8 @@ function widgetLoadFromRfc5545(form, conf, icaldata, force) {
                     break;
 
                 case "rirangeoptions":
-                    var rangeType = "NOENDDATE";
+                    // default value per configuration
+                    var rangeType = conf.hasRepeatForeverButton ? "NOENDDATE" : "BYOCCURRENCES";
 
                     if (count) {
                         rangeType = "BYOCCURRENCES";
