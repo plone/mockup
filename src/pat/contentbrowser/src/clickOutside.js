175ad72d252699cb888d01c9b95a0d8808cb67c3
MrTango <md@derico.de>
2024-03-07 14:51:43 +0100

    upload WIP

--- src/pat/contentbrowser/src/clickOutside.js
+++ src/pat/contentbrowser/src/clickOutside.js
@@ -1,7 +1,9 @@
 /** Dispatch event on click outside of node */
 export function clickOutside(node) {
     const handleClick = (event) => {
-        if (node && !node.contains(event.target) && !event.defaultPrevented) {
+        debugger
+        //if (node && !node.contains(event.target) && !event.defaultPrevented) {
+        if (node && !node.contains(event.target)) {
             node.dispatchEvent(new CustomEvent("click_outside", node));
         }
     };
