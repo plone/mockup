a130157c1dbf3028f58f8d903745823ffb3b98ed
Peter Mathis <peter.mathis@kombinat.at>
2024-03-14 16:57:20 +0100

    Fix multiple pattern occurrences on one page with context stores

---
+++ src/pat/contentbrowser/src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { getContext } from "svelte";
+    import ContentBrowser from "./ContentBrowser.svelte";
+    import SelectedItems from "./SelectedItems.svelte";
+    import {
+        setConfig,
+        setSelectedItems,
+        setSelectedUids,
+        setShowContentBrowser,
+    } from "./stores";
+
+    export let maxDepth;
+    export let basePath = "";
+    export let selectableTypes = [];
+    export let maximumSelectionSize = -1;
+    export let attributes;
+    export let separator;
+    export let selection = [];
+    export let vocabularyUrl;
+    export let fieldId;
+
+    // initialize context stores
+    setConfig();
+    setSelectedItems();
+    setShowContentBrowser();
+    setSelectedUids();
+
+    let config = getContext("config");
+    $config = {
+        maxDepth: maxDepth,
+        basePath: basePath,
+        selectableTypes: selectableTypes,
+        maximumSelectionSize: maximumSelectionSize,
+        attributes: attributes,
+        separator: separator,
+        selection: selection,
+        vocabularyUrl: vocabularyUrl,
+        fieldId: fieldId,
+    }
+
+    console.log(`Initialized App<${fieldId}> with config ${JSON.stringify($config)}`);
+</script>
+
+<ContentBrowser />
+<SelectedItems />
