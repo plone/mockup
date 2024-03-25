ea3baaca327ad863d422ea9f5c7529c5cbcb21a3
Peter Mathis <peter.mathis@kombinat.at>
2024-03-25 20:07:40 +0100

    svelte testsetup (draft)

---
+++ src/pat/contentbrowser/contentbrowser.test.js
@@ -0,0 +1,31 @@
+import "./contentbrowser";
+import registry from "@patternslib/patternslib/src/core/registry";
+import utils from "@patternslib/patternslib/src/core/utils";
+
+
+describe("Content Browser", () => {
+    beforeEach(() => {
+        let options = {
+            vocabularyUrl: "/contentbrowser-test.json",
+        }
+        document.body.innerHTML = `
+            <div id="contentbrowser-field">
+                <input type="text" value="" class="pat-contentbrowser" data-pat-contentbrowser="${JSON.stringify(options)}">
+            </div>
+        `;
+    });
+
+    afterEach(() => {
+        document.body.innerHTML = "";
+    });
+
+    it("create contentbrowser pattern without preselection", async function () {
+        expect(document.querySelectorAll(".content-browser-wrapper").length).toEqual(0);
+
+        registry.scan(document.body);
+        await utils.timeout(1);
+
+        expect(document.querySelectorAll(".content-browser-wrapper").length).toEqual(1);
+    });
+
+});
