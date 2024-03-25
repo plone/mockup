197d73f9cb10d72d7e8b0d5f1d5070afca636b51
Peter Mathis <peter.mathis@kombinat.at>
2024-03-25 20:07:40 +0100

    svelte testsetup (draft)

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 const path = require("path");
 const config = require("@patternslib/dev/jest.config.js");
 
+// config.setupFilesAfterEnv.push("./node_modules/@testing-library/jest-dom/extend-expect");
 config.setupFilesAfterEnv.push(path.resolve(__dirname, "./src/setup-tests.js"));
 config.transformIgnorePatterns = [
-    "/node_modules/(?!@patternslib/)(?!@plone/)(?!preact/)(?!screenfull/)(?!sinon/)(?!bootstrap/)(?!datatable/).+\\.[t|j]sx?$",
+    "/node_modules/(?!@patternslib/)(?!@plone/)(?!preact/)(?!screenfull/)(?!sinon/)(?!bootstrap/)(?!datatable/)(?!svelte/).+\\.[t|j]sx?$",
 ];
 
+// add svelte-jester
+config.transform["^.+\\.svelte$"] = "svelte-jester";
+
+console.log(JSON.stringify(config, null, 4));
 module.exports = config;
