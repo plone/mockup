42f9bb2060ef30ebcb5930780e6850a18215f037
Peter Mathis <peter.mathis@kombinat.at>
2024-05-08 16:27:40 +0200

    Implement level batching

--- src/pat/contentbrowser/src/ContentStore.js
+++ src/pat/contentbrowser/src/ContentStore.js
@@ -4,6 +4,22 @@ import { request } from "./api.js";
 export default function (config, pathCache) {
     const store = writable([]);
 
+    const load = async (query) => {
+        let defaults = {
+            vocabularyUrl: config.vocabularyUrl,
+            attributes: config.attributes,
+            pageSize: config.pageSize,
+        };
+        query = {
+            ...defaults,
+            ...query,
+        }
+        if (config.selectableTypes.length) {
+            query["selectableTypes"] = config.selectableTypes;
+        }
+        return await request(query);
+    }
+
     const browse = async (portalPath, path, searchTerm, updateCache) => {
         let paths = [];
         let parts = path.split("/") || [];
@@ -12,6 +28,7 @@ export default function (config, pathCache) {
         let partsToShow = parts.slice(parts.length - depth, parts.length);
         let partsToHide = parts.slice(0, parts.length - depth);
         const pathPrefix = portalPath + partsToHide.join("/");
+        const pC = get(pathCache);
 
         while (partsToShow.length > 0) {
             let sub_path = partsToShow.join("/").replace(/^\//, "");
@@ -26,32 +43,33 @@ export default function (config, pathCache) {
         for (var p of paths) {
             pathCounter++;
             const isFirstPath = pathCounter == 1;
-            let skipCache = isFirstPath && (searchTerm || updateCache);
             let level = {};
-            const c = get(pathCache);
-            if (Object.keys(c).indexOf(p) === -1 || skipCache) {
+            if (
+                !(p in pC) ||  // new path not found in cache
+                (isFirstPath && searchTerm) ||  // filtering the level
+                updateCache  // manual cache update request
+            ) {
                 let query = {
-                    method: "GET",
-                    vocabularyUrl: config.vocabularyUrl,
-                    attributes: config.attributes,
                     path: p,
                 };
 
                 if (isFirstPath && searchTerm) {
                     query["searchTerm"] = "*" + searchTerm + "*";
                 }
-                if (config.selectableTypes.length) {
-                    query["selectableTypes"] = config.selectableTypes;
-                }
-                level = await request(query);
+
+                level = await load(query);
+
+                // check if there is more than the current batch
+                level.more = config.pageSize < level.total;
+                // save possible search filter for later batch loading
+                level.searchTerm = searchTerm;
+                level.page = 1;
+                level.path = p;
 
                 // do not update cache when searching
-                if(!searchTerm) {
-                    const levelInfo = await request({
+                if (!searchTerm) {
+                    const levelInfo = await load({
                         levelInfoPath: p,
-                        vocabularyUrl: config.vocabularyUrl,
-                        attributes: config.attributes,
-
                     });
                     if (levelInfo.total) {
                         level.UID = levelInfo.results[0].UID;
@@ -60,50 +78,107 @@ export default function (config, pathCache) {
                         level.selectable = (!config.selectableTypes.length || config.selectableTypes.indexOf(levelInfo.results[0].portal_type) != -1);
                     }
                     level.gridView = false;
-                    level.path = p;
                     pathCache.update((n) => {
                         n[p] = level;
                         return n;
                     });
                 }
             } else {
-                level = c[p];
+                level = pC[p];
             }
             levels = [level, ...levels];
         }
         store.set(levels);
     }
 
-    const search = async (portalPath, searchTerm) => {
+    const search = async (portalPath, searchTerm, page) => {
         let query = {
-            method: "GET",
-            vocabularyUrl: config.vocabularyUrl,
-            attributes: config.attributes,
             searchPath: portalPath,
+            page: page,
         };
         if (searchTerm) {
             query["searchTerm"] = "*" + searchTerm + "*";
         }
-        if (config.selectableTypes.length) {
-            query["selectableTypes"] = config.selectableTypes;
+        let level = await load(query);
+        level.page = page;
+        level.searchTerm = searchTerm;
+
+        store.update((levels) => {
+            const has_more = (page * config.pageSize) < level.total;
+
+            // first time or new search
+            if (levels.length == 0 || levels[0].searchTerm != searchTerm) {
+                level.more = has_more;
+                level.selectable = false;
+                level.gridView = false;
+                return [level,];
+            }
+
+            // has more ?
+            levels[0].more = has_more;
+            levels[0].page = level.page;
+
+            // append new batch
+            levels[0].results = [
+                ...levels[0].results,
+                ...level.results,
+            ];
+            return levels;
+        });
+    }
+
+    const nextBatch = async (p, page, searchTerm) => {
+        let query = {
+            path: p,
+            page: page,
+        };
+
+        if (searchTerm) {
+            query["searchTerm"] = "*" + searchTerm + "*";
         }
-        let level = await request(query);
-        level.selectable = false;
-        level.gridView = false;
-        store.set([level, ]);
+
+        let level = await load(query);
+        level.more = (page * config.pageSize) < level.total;
+        level.page = page;
+
+        store.update((levels) => {
+            levels.forEach((l) => {
+                if (l.path != p) {
+                    return l;
+                }
+                l.page = level.page;
+                l.more = level.more;
+                l.results = [
+                    ...l.results,
+                    ...level.results,
+                ]
+            });
+            return levels;
+        });
     }
 
     store.get = async ({
         path = "",
         searchTerm = "",
         updateCache = false,
+        loadMorePath = "",
+        page = 1,
     }) => {
         const base_url = new URL(config.base_url);
         const portalPath = base_url.pathname;
 
-        if(config.mode === "search") {
-            await search(portalPath, searchTerm)
-        } else {
+        if (config.mode === "search") {
+            await search(portalPath, searchTerm, page)
+        } else if (loadMorePath) {
+            const pC = get(pathCache);
+            if (!(loadMorePath in pC)) {
+                return;
+            }
+            let level = pC[loadMorePath];
+            if (page > level.page) {
+                await nextBatch(loadMorePath, page, level.searchTerm);
+            }
+        } else if (path) {
             path = path.replace(new RegExp(`^${portalPath}`), "");
             await browse(portalPath, path, searchTerm, updateCache);
         }
