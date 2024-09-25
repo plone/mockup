fcb79ef4893bfe7cdaec1f2b3177ebe844cd5946
Johannes Raggam <thetetet@gmail.com>
2024-09-26 00:22:38 +0200

    feat(pat-navigationmarker): Modernize: class based Pattern, add option portal-url, no jQuery.

--- src/pat/navigationmarker/navigationmarker.js
+++ src/pat/navigationmarker/navigationmarker.js
@@ -1,52 +1,90 @@
-import $ from "jquery";
-import Base from "@patternslib/patternslib/src/core/base";
+import { BasePattern } from "@patternslib/patternslib/src/core/basepattern";
+import Parser from "@patternslib/patternslib/src/core/parser";
+import registry from "@patternslib/patternslib/src/core/registry";
 
-export default Base.extend({
-    name: "navigationmarker",
-    trigger: ".pat-navigationmarker",
-    parser: "mockup",
-    init: function () {
-        const portal_url = document.body.dataset.portalUrl;
-        var href =
-            document.querySelector('head link[rel="canonical"]').href ||
+export const parser = new Parser("navigationmarker");
+parser.addArgument("portal-url", undefined);
+
+class Pattern extends BasePattern {
+    static name = "navigationmarker";
+    static trigger = ".pat-navigationmarker";
+    static parser = parser;
+
+    async init() {
+        this.portal_url = this.options.portalUrl || document.body.dataset.portalUrl;
+
+        this.scan_navigation();
+    }
+
+    scan_navigation() {
+        const href =
+            document.querySelector('head link[rel="canonical"]')?.href ||
             window.location.href;
 
-        $("a", this.$el).each(function () {
-            var navlink = this.href.replace("/view", "");
-            if (href.indexOf(navlink) !== -1) {
-                var parent = $(this).parent();
+        const anchors = this.el.querySelectorAll("a");
 
-                // check the input-openers within the path
-                var check = parent.find("> input");
-                if (check.length) {
-                    check[0].checked = true;
-                }
+        for (const anchor of anchors) {
 
-                // set "inPath" to all nav items which are within the current path
-                // check if parts of navlink are in canonical url parts
-                var hrefParts = href.split("/");
-                var navParts = navlink.split("/");
-                var inPath = false;
-                for (var i = 0, size = navParts.length; i < size; i++) {
-                    // The last path-part must match.
-                    inPath = false;
-                    if (navParts[i] === hrefParts[i]) {
-                        inPath = true;
-                    }
-                }
-                if (navlink === portal_url && href !== portal_url) {
-                    // Avoid marking "Home" with "inPath", when not actually there
-                    inPath = false;
-                }
-                if (inPath) {
-                    parent.addClass("inPath");
-                }
+            const parent = anchor.parentElement;
+            const navlink = anchor.href.replace("/view", "");
 
-                // set "current" to the current selected nav item, if it is in the navigation structure.
-                if (href === navlink) {
-                    parent.addClass("current");
-                }
+            // We can exit early, if the navlink is not part of the current URL.
+            if (href.indexOf(navlink) === -1) {
+                this.clear(parent);
+                continue;
             }
-        });
-    },
-});
+
+            // BBB
+            // check the input-openers within the path
+            const check = parent.querySelector(":scope > input");
+            if (check) {
+                check.checked = true;
+            }
+
+            // set "inPath" to all nav items which are within the current path
+            // check if parts of navlink are in canonical url parts
+            //
+            const href_parts = href.split("/");
+            const nav_parts = navlink.split("/");
+            let inPath = false;
+
+            // The last part of the URL must match.
+            const nav_compare = nav_parts[nav_parts.length - 1];
+            const href_compare = href_parts[nav_parts.length - 1];
+            if (nav_compare === href_compare) {
+                inPath = true;
+            }
+
+            // Avoid marking "Home" with "inPath", when not actually there
+            if (navlink === this.portal_url && href !== this.portal_url) {
+                inPath = false;
+            }
+
+            // Set the class
+            if (inPath) {
+                parent.classList.add("inPath");
+            }
+
+            // set "current" to the current selected nav item, if it is in the navigation structure.
+            if (href === navlink) {
+                parent.classList.add("current");
+            }
+        }
+    }
+
+    clear(element) {
+        // Clear all classes
+        if (element.classList.contains("inPath")) {
+            element.classList.remove("inPath");
+        }
+        if (element.classList.contains("current")) {
+            element.classList.remove("current");
+        }
+    }
+}
+
+// Register Pattern class in the global pattern registry
+registry.register(Pattern);
+
+// Make it available
+export default Pattern;
