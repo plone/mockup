fc5fd9b62065481979b0f368ec5a158750da6607
Johannes Raggam <thetetet@gmail.com>
2024-10-25 02:14:30 +0200

    feat(pat-toolbar): Reload itself on location change.

--- src/pat/toolbar/toolbar.js
+++ src/pat/toolbar/toolbar.js
@@ -1,5 +1,6 @@
 import $ from "jquery";
 import { BasePattern } from "@patternslib/patternslib/src/core/basepattern";
+import events from "@patternslib/patternslib/src/core/events";
 import Parser from "@patternslib/patternslib/src/core/parser";
 import registry from "@patternslib/patternslib/src/core/registry";
 import Cookies from "js-cookie";
@@ -17,15 +18,37 @@ class Pattern extends BasePattern {
 
     async init() {
         if (window.__patternslib_import_styles) {
-            import("./toolbar.scss");
+            //import("./toolbar.scss");
         }
 
-        // Reload on context change in folder content browser.
-        $("body").on(this.options["update-trigger"], async (e, path) => {
-            await this.reload_toolbar(
-                `${document.body.dataset["portal-url"]}${path}/${this.options["render-url"]}`
-            );
-        });
+        //// Reload on context change in folder content browser.
+        //$("body").on(this.options["update-trigger"], async (e, path) => {
+        //    await this.reload_toolbar(
+        //        `${document.body.dataset.portalUrl}${path}/${this.options["render-url"]}`
+        //    );
+        //});
+
+        // Reload the toolbar on history change.
+        events.add_event_listener(
+            window.navigation,
+            "navigate",
+            "pat-toolbar--history-changed",
+            async () => {
+                const url = `${document.body.dataset.baseUrl}/${this.options["render-url"]}`;
+                await this.reload_toolbar(url);
+            }
+        );
+
+        //events.add_event_listener(
+        //    document.body,
+        //    "pat-inject-history-changed",
+        //    "pat-toolbar--history-changed",
+        //    async (e) => {
+        //        await this.reload_toolbar(
+        //            `${e.detail.url}/${this.options["render-url"]}`
+        //        );
+        //    }
+        //);
 
         const $el = $(this.el);
 
@@ -62,8 +85,8 @@ class Pattern extends BasePattern {
         // setup modals
         registry.scan(main_toolbar);
         registry.scan(personal_tools);
-        document.querySelector(".plone-toolbar-main").replaceWith(main_toolbar);
-        document.querySelector("#collapse-personaltools").replaceWith(personal_tools);
+        document.querySelector(".plone-toolbar-main")?.replaceWith(main_toolbar);
+        document.querySelector("#collapse-personaltools")?.replaceWith(personal_tools);
 
         // Notify others that the toolbar has been reloaded.
         this.el.dispatchEvent(events.generic_event("pat-toolbar--reloaded"));
