5fc09ab756de9b0b87c9ae7ee4198ecf9ea8b767
ninanoleto <marinaa.noleto@gmail.com>
2025-02-13 00:17:12 +0100

    feat: WIP add svg css and xml support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,43 @@
+const path = require("path");
+
 module.exports = {
-    stories: ["../src/stories/**/*.docs.mdx", "../src/stories/**/*.stories.js"],
+    stories: ["../src/pat/**/*.docs.mdx", "../src/pat/**/*.stories.js"],
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
         "@storybook/addon-docs",
+        "storybook-addon-mock",
     ],
     framework: {
         name: "@storybook/html-webpack5",
         options: {},
+    },
+    webpackFinal: async (config) => {
+        if (!config.resolve) config.resolve = {};
+        if (!config.resolve.alias) config.resolve.alias = {};
+
+        // Support XML
+        config.module.rules.push({
+            test: /\.xml$/,
+            type: "asset/resource",
+            generator: {
+                filename: "static/assets/[name][ext]",
+            },
+        });
+
+        // Support SCSS
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"],
+        });
+
+        // Support SVG
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
     },
 };
