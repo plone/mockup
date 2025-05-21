f2e2cb2da62c1cfbaa32a2dc925e707cf151db3a
Johannes Raggam <thetetet@gmail.com>
2025-05-22 00:04:10 +0200

    Update src/pat/tinymce/tinymce--implementation.js
    Co-authored-by: Peter Mathis <petschki@users.noreply.github.com>

--- src/pat/tinymce/tinymce--implementation.js
+++ src/pat/tinymce/tinymce--implementation.js
@@ -283,7 +283,7 @@ export default class TinyMCE {
         // from `content_css` which should be used to automatically create the
         // styles dropdown.
         // Also see:
-        // https://www.tiny.cloud/docs/tinymce/latest/importcss/#importcss_file_filter
+        // https://6.docs.plone.org/classic-ui/tinymce-customization.html#inject-formats-with-files-named-tinymce-formats-css
         if (
             tinyOptions.importcss_file_filter &&
             typeof tinyOptions.importcss_file_filter.indexOf === "function" &&
