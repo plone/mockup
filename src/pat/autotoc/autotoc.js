937db881b86a5cee9e7e97c685226fbdff0b0ab8
Cihan Andac <cihanandac@gmail.com>
2025-05-27 14:31:55 +0200

    feat: make markup compatible with the bootstrap

--- src/pat/autotoc/autotoc.js
+++ src/pat/autotoc/autotoc.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import Base from "@patternslib/patternslib/src/core/base";
 import utils from "@patternslib/patternslib/src/core/utils";
+import Tab from "bootstrap/js/dist/tab";
 
 export default Base.extend({
     name: "autotoc",
@@ -10,8 +11,9 @@ export default Base.extend({
         section: "section",
         levels: "h1,h2,h3",
         IDPrefix: "autotoc-item-",
-        classTOCName: "autotoc-nav",
-        classSectionName: "autotoc-section",
+        classTOCName: "autotoc-nav nav",
+        classContentAreaName: "autotoc-content tab-content",
+        classSectionName: "autotoc-section tab-pane fade",
         classLevelPrefixName: "autotoc-level-",
         classActiveName: "active",
         scrollDuration: "slow",
@@ -26,7 +28,9 @@ export default Base.extend({
 
         var self = this;
 
-        self.$toc = $("<nav/>").addClass(self.options.classTOCName);
+        self.$toc = $("<ul/>")
+            .addClass(self.options.classTOCName)
+            .attr("role", "tablist");
 
         if (self.options.prependTo) {
             self.$toc.prependTo(self.options.prependTo);
@@ -36,13 +40,26 @@ export default Base.extend({
             self.$toc.prependTo(self.$el);
         }
 
+
         if (self.options.className) {
             self.$el.addClass(self.options.className);
         }
 
-        $(self.options.section, self.$el).addClass(self.options.classSectionName);
+        $(self.options.section, self.$el)
+            .addClass(self.options.classSectionName)
+            .attr("role", "tabpanel")
+            .attr("tabindex", "0")
 
         var asTabs = self.$el.hasClass("autotabs");
+
+        if (asTabs) {
+            self.$toc.addClass("nav-tabs");
+            self.$contentArea = $("<div/>").addClass(self.options.classContentAreaName);
+            self.$contentArea.insertAfter(self.$toc);
+            $(self.options.section, self.$el).appendTo(self.$contentArea);
+        } else {
+            self.$toc.addClass("flex-column");
+        }
 
         var activeId = null;
 
@@ -54,39 +71,43 @@ export default Base.extend({
             if (!id || $("#" + id).length > 0) {
                 id = self.options.IDPrefix + self.name + "-" + i;
             }
+
+            const tabId = `${id}-tab`;
+            $(section).attr("id", id).attr("aria-labelledby", tabId);
+
             if (window.location.hash === "#" + id) {
-                activeId = id;
+                activeId = tabId;
             }
             if (activeId === null && $level.hasClass(self.options.classActiveName)) {
-                activeId = id;
+                activeId = tabId;
             }
             $level.data("navref", id);
+
+            const $navItem = $("<li/>");
+            $navItem
+                .addClass("nav-item")
+                .attr("role", "presentation")
+                .appendTo(self.$toc);
+
             const $nav = $("<a/>");
-            $nav.appendTo(self.$toc)
+            $nav.appendTo($navItem)
                 .text($level.text())
-                .attr("id", id)
-                .attr("href", "#" + id)
-                .addClass(self.options.classLevelPrefixName + self.getLevel($level))
+                .attr("id", tabId)
+                .attr("href", "#" + tabId)
+                .attr("aria-controls", id)
+                .attr("data-bs-toggle", "tab")
+                .attr("data-bs-target", `#${id}`)
+                .addClass([
+                    "nav-link",
+                    self.options.classLevelPrefixName + self.getLevel($level),
+                ])
                 .on("click", function (e, options) {
-                    e.stopPropagation();
-                    e.preventDefault();
                     if (!options) {
                         options = {
                             doScroll: true,
                             skipHash: false,
                         };
                     }
-                    var $el = $(this);
-                    self.$toc
-                        .children("." + self.options.classActiveName)
-                        .removeClass(self.options.classActiveName);
-                    self.$el
-                        .children("." + self.options.classActiveName)
-                        .removeClass(self.options.classActiveName);
-                    $(e.target).addClass(self.options.classActiveName);
-                    $level
-                        .parents(self.options.section)
-                        .addClass(self.options.classActiveName);
                     if (
                         options.doScroll !== false &&
                         self.options.scrollDuration &&
@@ -107,7 +128,7 @@ export default Base.extend({
                     $(this).trigger("clicked");
                     if (!options.skipHash) {
                         if (window.history && window.history.pushState) {
-                            window.history.pushState({}, "", "#" + $el.attr("id"));
+                            window.history.pushState({}, "", `#${id}`);
                         }
                     }
                 });
@@ -121,15 +142,17 @@ export default Base.extend({
         });
 
         if (activeId) {
-            $("a#" + activeId).trigger("click", {
-                doScroll: true,
-                skipHash: true,
-            });
+            const activeTabButton = self.$toc.find("a#" + activeId)[0];
+            if (activeTabButton) {
+                const tab = Tab.getOrCreateInstance(activeTabButton);
+                tab.show();
+            }
         } else {
-            self.$toc.find("a").first().trigger("click", {
-                doScroll: false,
-                skipHash: true,
-            });
+            const firstTabButton = self.$toc.find("a").first()[0];
+            if (firstTabButton) {
+                const tab = Tab.getOrCreateInstance(firstTabButton);
+                tab.show();
+            }
         }
 
         // After DOM tree is built, initialize eventual validation
