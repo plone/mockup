d4c8ebfe9e7553721e5bf69fcc1d4934c4247c91
Peter Mathis <peter.mathis@kombinat.at>
2026-01-10 18:18:30 +0100

    fix release-it requireBranch config

--- .release-it.js
+++ .release-it.js
@@ -1,1 +1,6 @@
-module.exports = require("@patternslib/dev/.release-it.js");
+const ri_config = require("@patternslib/dev/.release-it.js");
+
+module.exports = () => {
+    ri_config.git.requireBranch = "5.4.x";
+    return ri_config;
+};
