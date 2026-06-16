8b30efd832bfc8608501dc09f18e57858f5468a5
Peter Mathis <peter.mathis@kombinat.at>
2026-06-16 15:34:54 +0200

    maint(bundle): update release-it config for maintenance branch

--- .release-it.js
+++ .release-it.js
@@ -1,1 +1,6 @@
-module.exports = require("@patternslib/dev/.release-it.js");
+const config = require("@patternslib/dev/.release-it.js");
+
+// Releasing from the maintenance branch instead of master/main.
+config.git.requireBranch = "5.6.x";
+
+module.exports = config;
